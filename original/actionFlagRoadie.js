7da7e904d96d7aa550ef6a4539fbe482047a6c33
LaustN <laustn@gmail.com>
2016-03-21 22:19:32 +0100

    debugging roadie

--- original/actionFlagRoadie.js
+++ original/actionFlagRoadie.js
@@ -1,48 +1,45 @@
 module.exports = function(creep){
-    if(!creep.memory.waypoints || creep.memory.waypoints.length == 0) {
-        creep.memory.waypoints = [];
-        creep.memory.waypoints.push(creep.memory.home);
+  if(!creep.memory.waypoints || creep.memory.waypoints.length == 0) {
+    creep.memory.waypoints = [];
+    creep.memory.waypoints.push(creep.memory.home);
 
-        var localFlags = creep.room.find(FIND_FLAGS);
-        for(var flagName in localFlags){
-            var flag = localFlags[flagName].id;
-            creep.memory.waypoints.push(flag);
-            creep.memory.waypoints.push(creep.memory.home);
-        }
+    var localFlags = creep.room.find(FIND_FLAGS);
+    for(var flagName in localFlags){
+      var flag = localFlags[flagName].id;
+      creep.memory.waypoints.push(flag);
+      creep.memory.waypoints.push(creep.memory.home);
     }
+  }
 
-    var roadFound = false;
-    var lookResult = creep.pos.look();
-    if(lookResult) {
-      console.log("roadie has a lookResult");
-        for(var lookItemName in lookResult){
-            var lookItem = lookResult[lookItemName];
+  var roadFound = false;
+  var lookResult = creep.pos.look();
+  if(lookResult) {
+    console.log("roadie has a lookResult");
+    for(var lookItemName in lookResult){
+      var lookItem = lookResult[lookItemName];
 
-            if(lookItem.type == "structure" && lookItem.structure.structureType == STRUCTURE_ROAD){
-              console.log("Road Found");
-                roadFound = true;
-            }
-        }
-    }
-    var nearbySites = creep.pos.findInRange(FIND_CONSTRUCTION_SITES,0)
-    if(nearbySites && nearbySites.length>0 ){
+      if(lookItem.type == "structure" && lookItem.structure.structureType == STRUCTURE_ROAD){
+        console.log("Road Found");
         roadFound = true;
+      }
     }
-    if(!roadFound){
-        creep.pos.createConstructionSite(STRUCTURE_ROAD);
-        console.log(creep.name + " creating road here");
-    }
+  }
+  var nearbySites = creep.pos.findInRange(FIND_CONSTRUCTION_SITES,0)
+  if(nearbySites && nearbySites.length>0 ){
+    roadFound = true;
+  }
+  if(!roadFound){
+    creep.pos.createConstructionSite(STRUCTURE_ROAD);
+    console.log(creep.name + " creating road here");
+  }
 
-    var waypoint = Game.getObjectById(creep.memory.waypoints[0]);
-    while(waypoint == null && creep.memory.waypoints.length>0){
-        creep.memory.waypoints.shift();
-        waypoint = Game.getObjectById(creep.memory.waypoints[0]);
-
-    }
-    creep.moveTo(waypoint);
-    if(creep.pos.getRangeTo(waypoint) <= 1){
-        creep.memory.waypoints.shift();
-    }
-
-
+  var waypoint = Game.getObjectById(creep.memory.waypoints[0]);
+  while(waypoint == null && creep.memory.waypoints.length>0){
+    creep.memory.waypoints.shift();
+    waypoint = Game.getObjectById(creep.memory.waypoints[0]);
+  }
+  creep.moveTo(waypoint);
+  if(creep.pos.getRangeTo(waypoint) <= 1){
+    creep.memory.waypoints.shift();
+  }
 }
