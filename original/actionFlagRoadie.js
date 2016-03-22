b4ef1be74bdf1c56e22549306973aeebd34d3acf
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-03-22 08:35:38 +0100

    more logging

--- original/actionFlagRoadie.js
+++ original/actionFlagRoadie.js
@@ -11,7 +11,7 @@ module.exports = function(creep){
     }
   }
   creep.pos.look().forEach(function(elementAtPostion){
-    console.log("FooBar");
+    console.log(elementAtPostion);
   });
 
   var roadFound = false;
