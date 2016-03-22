4c05f34bd7ee91ef4e00d0cd972e39cce9ed09cd
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-03-22 09:50:42 +0100

    running creepManager in foreach

--- original/main.js
+++ original/main.js
@@ -11,32 +11,34 @@ var ensureHome = require("actionEnsureHome");
 var runTowers = require("runTowers");
 
 module.exports.loop = function () {
-    creepManager(Game.spawns.Spawn1);
-    runTowers();
+  for(var spawnName in Game.spawns){
+    creepManager(Game.spawns[spawnName]);
+  }
+  runTowers();
 
-    for(var creepName in Game.creeps){
-        var creep = Game.creeps[creepName];
-        ensureHome(creep);
-        if(creep.memory.role == 'harvester'){
-            harvester(creep);
-        }
-        if(creep.memory.role == 'builder'){
-            builder(creep);
-        }
-        if(creep.memory.role == 'fortifier'){
-            fortifier(creep);
-        }
-        if(creep.memory.role == 'guard') {
-            guard(creep);
-        }
-        if(creep.memory.role == 'healer') {
-            healer(creep);
-        }
-        if(creep.memory.role == 'controlUpgrader') {
-            controlUpgrader(creep);
-        }
-        if(creep.memory.role == 'roadie') {
-            roadie(creep);
-        }
+  for(var creepName in Game.creeps){
+    var creep = Game.creeps[creepName];
+    ensureHome(creep);
+    if(creep.memory.role == 'harvester'){
+      harvester(creep);
     }
+    if(creep.memory.role == 'builder'){
+      builder(creep);
+    }
+    if(creep.memory.role == 'fortifier'){
+      fortifier(creep);
+    }
+    if(creep.memory.role == 'guard') {
+      guard(creep);
+    }
+    if(creep.memory.role == 'healer') {
+      healer(creep);
+    }
+    if(creep.memory.role == 'controlUpgrader') {
+      controlUpgrader(creep);
+    }
+    if(creep.memory.role == 'roadie') {
+      roadie(creep);
+    }
+  }
 }
