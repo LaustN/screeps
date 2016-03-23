8251ef6234484025c3b08faf2af922ee0d23b06c
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-03-23 13:48:16 +0100

    figuring out why harvesters dislike northern point

--- original/actionHarvest.js
+++ original/actionHarvest.js
@@ -1,22 +1,23 @@
 module.exports = function(creep){
-    var harvestTarget = creep.room.find(FIND_SOURCES)[0];
+  var harvestTarget = creep.room.find(FIND_SOURCES)[0];
 
-    if(creep.memory.focus) {
-        var focus = Game.getObjectById(creep.memory.focus);
-        if(focus){
-            harvestTarget = focus.pos.findClosestByRange(FIND_SOURCES);
-        }
+  if(creep.memory.focus) {
+    var focus = Game.getObjectById(creep.memory.focus);
+    if(focus){
+      harvestTarget = focus.pos.findClosestByRange(FIND_SOURCES);
     }
+  }
 
-	var harvestMessage =  creep.harvest(harvestTarget);
-	if(harvestMessage  == ERR_NOT_IN_RANGE) {
-	    var moveMessage = creep.moveTo(harvestTarget)
-		if(moveMessage != OK){
-            var alternativeSource =  creep.pos.findClosestByRange(FIND_SOURCES,{ filter: function(source){ return source.id != harvestTarget.id }});
-            if(alternativeSource) {
-                creep.memory.focus = alternativeSource.id;
-            }
+  var harvestMessage =  creep.harvest(harvestTarget);
+  if(harvestMessage  == ERR_NOT_IN_RANGE) {
+    var moveMessage = creep.moveTo(harvestTarget)
+    if(moveMessage != OK){
+      var alternativeSource =  creep.pos.findClosestByRange(FIND_SOURCES,{ filter: function(source){ return source.id != harvestTarget.id }});
+      if(alternativeSource) {
+        console.log(creep.name + " is now targeting " + alternativeSource.pos);
+        creep.memory.focus = alternativeSource.id;
+      }
 
-		}
-	}
+    }
+  }
 }
