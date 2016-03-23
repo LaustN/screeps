0bf4e2793828591ee090de233b95c7f8b5c5a86c
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-03-23 09:41:33 +0100

    allowing collection of energy directly from nearby harvesters and  trucks

--- original/actionCollectEnergy.js
+++ original/actionCollectEnergy.js
@@ -1,34 +1,53 @@
 module.exports = function(creep){
+  var home = Game.getObjectById(creep.memory.home);
+  if(home.memory.state != "SaveEnergy") {
     var collectionPointsHere = creep.pos.findInRange(FIND_MY_STRUCTURES,1,{
-        filter: function(structure){
-            structure.energy > 0;
-        }
+      filter: function(structure){
+        structure.energy > 0;
+      }
     });
     if(collectionPointsHere && collectionPointsHere[0]){
-	    var home = Game.getObjectById(creep.memory.home);
-    	if(home.memory.state != "SaveEnergy") {
-            collectionPointsHere[0].transferEnergy(creep);
-    	}
+      collectionPointsHere[0].transferEnergy(creep);
+      return true;
     }
 
-	if(creep.carry.energy == 0) {
-	    creep.memory.collectingEnergy = true;
-	}
-
-	if(creep.carry.energy == creep.carryCapacity) {
-	    creep.memory.collectingEnergy = false;
-	}
-
-	if(creep.memory.collectingEnergy) {
-	    var home = Game.getObjectById(creep.memory.home);
-    	if(home.memory.state != "SaveEnergy") {
-            creep.moveTo(home);
-    		home.transferEnergy(creep);
-    	}
-    	else{
-    	    var actionHarvest = require("actionHarvest");
-    	    actionHarvest(creep);
-    	}
-    	return true;
+    var energyCarriersHere = creep.pos.findInRange(FIND_MY_CREEPS,1,{
+      filter: function(harvestCreep){
+        if (harvestCreep.memory.role == "harvestTruck" && harvestCreep.carry.energy > 0 ) {
+          return true;
+        };
+        if (harvestCreep.memory.role == "harvester" && harvestCreep.carry.energy > 0 ) {
+          return true;
+        };
+      }
+    });
+    if(energyCarriersHere && energyCarriersHere[0])
+    {
+      console.log(creep.name + " is getting some energy from " + energyCarriersHere[0].name);
+      energyCarriersHere[0].transferEnergy(creep);
+      return true;
     }
+
+  }
+
+  if(creep.carry.energy == 0) {
+    creep.memory.collectingEnergy = true;
+  }
+
+  if(creep.carry.energy == creep.carryCapacity) {
+    creep.memory.collectingEnergy = false;
+  }
+
+  if(creep.memory.collectingEnergy) {
+    var home = Game.getObjectById(creep.memory.home);
+    if(home.memory.state != "SaveEnergy") {
+      creep.moveTo(home);
+      home.transferEnergy(creep);
+    }
+    else{
+      var actionHarvest = require("actionHarvest");
+      actionHarvest(creep);
+    }
+    return true;
+  }
 }
