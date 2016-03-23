615b100e32114791e3f7cda0214c3920a6d82c33
LaustN <laustn@gmail.com>
2016-03-23 22:39:09 +0100

    more scouting debug

--- original/actionScout.js
+++ original/actionScout.js
@@ -1,15 +1,10 @@
 module.exports = function(creep){
   if(creep.memory.scoutTarget){
-    creep.say("scouting");
     var scoutTarget = Game.getObjectById(creep.memory.scoutTarget);
-    console.log("I found this scoutTarget:" + scoutTarget);
     if(scoutTarget){
-
       if(creep.pos.roomName == scoutTarget.pos.roomName){
-        console.log("I am in the room now!" + scoutTarget);
         return false;
       }
-
       creep.moveTo(scoutTarget);
       return true;
     }
