ab86cfc77117290e9c57e53203a51572d6628085
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-03-23 14:45:20 +0100

    refactor on collectEnergy, should permit collection from extensions

--- original/actionCollectEnergy.js
+++ original/actionCollectEnergy.js
@@ -1,41 +1,4 @@
 module.exports = function(creep){
-  var home = Game.getObjectById(creep.memory.home);
-  if(home.memory.state != "SaveEnergy") {
-    var collectionPointsHere = creep.pos.findInRange(FIND_MY_STRUCTURES,1,{
-      filter: function(structure){
-        structure.energy > 0;
-      }
-    });
-    if(collectionPointsHere && collectionPointsHere[0]){
-      collectionPointsHere[0].transferEnergy(creep);
-      return true;
-    }
-
-    var energyCarriersHere = creep.pos.findInRange(FIND_MY_CREEPS,1,{
-      filter: function(harvestCreep){
-        if (harvestCreep.memory.role == "harvestTruck" && harvestCreep.carry.energy > 0 ) {
-          return true;
-        };
-        if (harvestCreep.memory.role == "harvester" && harvestCreep.carry.energy > 0 ) {
-          return true;
-        };
-      }
-    });
-
-    if(energyCarriersHere && energyCarriersHere[0])
-    {
-      var bestCarrier = energyCarriersHere[0]
-      for(var energyCarrierKey in energyCarriersHere){
-        var currentCarrier = energyCarriersHere[energyCarrierKey];
-        if(currentCarrier.carry.energy > bestCarrier.carry.energy){
-          bestCarrier = currentCarrier;
-        }
-      }
-      console.log(creep.name + " is getting some energy from " + bestCarrier.name);
-      bestCarrier.transferEnergy(creep);
-      return true;
-    }
-  }
   if(creep.carry.energy == 0) {
     creep.memory.collectingEnergy = true;
   }
@@ -47,13 +10,44 @@ module.exports = function(creep){
   if(creep.memory.collectingEnergy) {
     var home = Game.getObjectById(creep.memory.home);
     if(home.memory.state != "SaveEnergy") {
-      creep.moveTo(home);
-      home.transferEnergy(creep);
+      var collectionPointsHere = creep.pos.findInRange(FIND_MY_STRUCTURES,1,{
+        filter: function(structure){
+          return structure.energy > 0;
+        }
+      });
+      if(collectionPointsHere && collectionPointsHere[0]){
+        collectionPointsHere[0].transferEnergy(creep);
+        return true;
+      }
+
+      var energyCarriersHere = creep.pos.findInRange(FIND_MY_CREEPS,1,{
+        filter: function(harvestCreep){
+          if (harvestCreep.memory.role == "harvestTruck" && harvestCreep.carry.energy > 0 ) {
+            return true;
+          };
+          if (harvestCreep.memory.role == "harvester" && harvestCreep.carry.energy > 0 ) {
+            return true;
+          };
+        }
+      });
+
+      if(energyCarriersHere && energyCarriersHere[0])
+      {
+        var bestCarrier = energyCarriersHere[0]
+        for(var energyCarrierKey in energyCarriersHere){
+          var currentCarrier = energyCarriersHere[energyCarrierKey];
+          if(currentCarrier.carry.energy > bestCarrier.carry.energy){
+            bestCarrier = currentCarrier;
+          }
+        }
+        console.log(creep.name + " is getting some energy from " + bestCarrier.name);
+        bestCarrier.transferEnergy(creep);
+        return true;
+      }
     }
-    else{
-      var actionHarvest = require("actionHarvest");
-      actionHarvest(creep);
-    }
+
+    creep.moveTo(home);
+    home.transferEnergy(creep);
     return true;
   }
 }
