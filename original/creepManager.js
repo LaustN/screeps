5954aa65292588b644a3e1b3dd4a7187b3d7227e
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-03-30 09:32:12 +0200

    making creep re-find more effective

--- original/creepManager.js
+++ original/creepManager.js
@@ -137,75 +137,76 @@ module.exports = function (spawn) {
     },
     /* battle force removed do free more cash up for upgrading
     {
-      body: [TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,ATTACK,ATTACK,RANGED_ATTACK],
-      name: "Guard1",
-      memory: {
-        role: "guard"
-      }
-    },
-    {
-      body: [TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,ATTACK,ATTACK,RANGED_ATTACK],
-      name: "Guard2",
-      memory: {
-        role: "guard"
-      }
-    },
-    {
-      body: [TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,ATTACK,ATTACK,RANGED_ATTACK],
-      name: "Guard3",
-      memory: {
-        role: "guard"
-      }
-    },
-    {
-      body: [TOUGH,TOUGH,TOUGH,TOUGH,HEAL,HEAL,MOVE,MOVE],
-      name: "Healer1",
-      memory: {
-        role: "healer"
-      }
-    },*/
-    {
-      body: [CARRY,CARRY,CARRY,CARRY,MOVE,MOVE],
-      name: "Roadie1",
-      memory: {
-        role: "roadie",
-        scavengeRange: 100
-      }
-    },
-    {
-      body: [CARRY,CARRY,CARRY,CARRY,MOVE,MOVE],
-      name: "Redistributor1",
-      memory: {
-        role: "redistributor"
-      }
-    },
-  ];
-  for(var creepNumber in creepsToMaintain){
-    var creepDefinition = creepsToMaintain[creepNumber];
-    var preExistingCreep =
-    spawn.room.find(FIND_MY_CREEPS,
-      {filter:
-        function(foundCreep){
-          return foundCreep.name == creepDefinition.name;
-        }
-      });
-      if(!preExistingCreep[0]){
-        var createMessage = spawn.createCreep(creepDefinition.body,creepDefinition.name,creepDefinition.memory);
-        if(createMessage == creepDefinition.name){
-          spawn.memory.state = "OK";
-          console.log("Respawning " + creepDefinition.name);
-        }
-        else if(createMessage == ERR_NOT_ENOUGH_RESOURCES){
-          spawn.memory.state = "SaveEnergy";
-          console.log("Saving up for " + creepDefinition.name);
-        }
+    body: [TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,ATTACK,ATTACK,RANGED_ATTACK],
+    name: "Guard1",
+    memory: {
+    role: "guard"
+  }
+},
+{
+body: [TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,ATTACK,ATTACK,RANGED_ATTACK],
+name: "Guard2",
+memory: {
+role: "guard"
+}
+},
+{
+body: [TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,ATTACK,ATTACK,RANGED_ATTACK],
+name: "Guard3",
+memory: {
+role: "guard"
+}
+},
+{
+body: [TOUGH,TOUGH,TOUGH,TOUGH,HEAL,HEAL,MOVE,MOVE],
+name: "Healer1",
+memory: {
+role: "healer"
+}
+},*/
+{
+  body: [CARRY,CARRY,CARRY,CARRY,MOVE,MOVE],
+  name: "Roadie1",
+  memory: {
+    role: "roadie",
+    scavengeRange: 100
+  }
+},
+{
+  body: [CARRY,CARRY,CARRY,CARRY,MOVE,MOVE],
+  name: "Redistributor1",
+  memory: {
+    role: "redistributor"
+  }
+},
+];
+for(var creepNumber in creepsToMaintain){
+  var creepDefinition = creepsToMaintain[creepNumber];
+  var preExistingCreep = Game.creeps[creepDefinition.name];
 
-        return;
-      }
-      else{
-        spawn.memory.state = "OK";
-      }
-
+  // spawn.room.find(FIND_MY_CREEPS,
+  //   {filter:
+  //     function(foundCreep){
+  //       return foundCreep.name == creepDefinition.name;
+  //     }
+  //   });
+  if(!preExistingCreep){
+    var createMessage = spawn.createCreep(creepDefinition.body,creepDefinition.name,creepDefinition.memory);
+    if(createMessage == creepDefinition.name){
+      spawn.memory.state = "OK";
+      console.log("Respawning " + creepDefinition.name);
+    }
+    else if(createMessage == ERR_NOT_ENOUGH_RESOURCES){
+      spawn.memory.state = "SaveEnergy";
+      console.log("Saving up for " + creepDefinition.name);
     }
 
+    return;
   }
+  else{
+    spawn.memory.state = "OK";
+  }
+
+}
+
+}
