9ae03a4e7f87f7bac1b166de1c5d47039c9468ca
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-03-30 12:15:51 +0200

    logging structures for H6

--- original/actionUnloadEnergy.js
+++ original/actionUnloadEnergy.js
@@ -1,57 +1,49 @@
 module.exports = function(creep){
-    var dropPointsHere = creep.pos.findInRange(FIND_MY_STRUCTURES,1,{
-        filter: function(structure){
-            return structure.energyCapacity > structure.energy;
+  var dropPointsHere = creep.pos.findInRange(FIND_MY_STRUCTURES,1,{
+    filter: function(structure){
+      return structure.energyCapacity > structure.energy;
+    }
+  });
+  if(dropPointsHere){
+    creep.transferEnergy(dropPointsHere[0]);
+  }
+
+  if(creep.carry.energy == 0){
+    creep.memory.dropoff = false;
+  }
+  if(creep.carry.energy == creep.carryCapacity){
+    creep.memory.dropoff = true;
+  }
+
+  if(creep.memory.dropoff == true){
+    if(creep.name == "Harvest6"){
+      console.log("dropping off");
+    }
+
+
+    var dropoff = creep.room.find(FIND_MY_STRUCTURES,{
+      filter: function(structure){
+        console.log("filtering structures");
+
+        if (structure.energyCapacity && structure.energy && structure.energyCapacity > structure.energy) {
+          console.log("true case 1");
+          return true;
+        };
+
+        if(structure.storeCapacity && _.sum(structure.store) < structure.storeCapacity){
+          console.log("true case 2");
+          return true;
         }
-    });
-    if(dropPointsHere){
-        creep.transferEnergy(dropPointsHere[0]);
-    }
-
-    if(creep.carry.energy == 0){
-        creep.memory.dropoff = false;
-    }
-    if(creep.carry.energy == creep.carryCapacity){
-        creep.memory.dropoff = true;
-    }
-
-    if(creep.memory.dropoff == true){
-      if(creep.name == "Harvest6"){
-        console.log("dropping off");
+        console.log("false case 1");
+        return false;
       }
+    });
 
-
-	    var dropoff = creep.room.find(FIND_MY_STRUCTURES,{
-	        filter: function(structure){
-            if(creep.name == "Harvest6"){
-              console.log("filtering structures");
-            }
-
-	            if (structure.energyCapacity && structure.energy && structure.energyCapacity > structure.energy) {
-                if(creep.name == "Harvest6"){
-                  console.log("true case 1");
-                }
-	              return true;
-	            };
-
-              if(structure.storeCapacity && _.sum(structure.store) < structure.storeCapacity){
-                if(creep.name == "Harvest6"){
-                  console.log("true case 2");
-                }
-                return true;
-              }
-              if(creep.name == "Harvest6"){
-                console.log("false case 1");
-              }
-              return false;
-	        }
-	    });
-
-	    if(dropoff && dropoff.length>0)
-	    {
-	        creep.moveTo(dropoff[0]);
-	        creep.transferEnergy(dropoff[0]);
-	    }
-        return true;
+    if(dropoff && dropoff.length>0)
+    {
+      creep.moveTo(dropoff[0]);
+      creep.transferEnergy(dropoff[0]);
     }
+    return true;
+  }
 }
