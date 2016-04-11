c4977107ce7ad1bd19799987c8655d46e6a13b67
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-04-11 13:26:50 +0200

    fucked over by towers giving out their energy too easily

--- original/actionFlee.js
+++ original/actionFlee.js
@@ -1,20 +1,15 @@
 module.exports = function(creep){
-    var hostiles = creep.pos.findInRange(FIND_HOSTILE_CREEPS,5);
+  var hostiles = creep.pos.findInRange(FIND_HOSTILE_CREEPS,5);
 
-    if(hostiles.length > 0){
-        creep.memory.fleeing = true;
+  var flee = hostiles.length > 0;
+  if(flee){
+    var ensurehome = require("actionEnsureHome");
+    ensurehome(creep);
+    var destination = Game.getObjectById(creep.memory.home);
+    if(creep.pos.getRangeTo(destination) <= 2){
+        return false;
     }
-
-    if(creep.memory.fleeing){
-        var ensurehome = require("actionEnsureHome");
-        ensurehome(creep);
-        var destination = Game.getObjectById(creep.memory.home);
-        if(creep.pos.getRangeTo(destination) <= 1){
-            creep.memory.fleeing = false;
-            return false;
-        }
-        creep.moveTo(destination);
-        return true;
-    }
-    return false;
+    creep.moveTo(destination);
+    return true;
+  }
 }
