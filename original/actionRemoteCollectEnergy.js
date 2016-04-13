df9bd486e8391803b9a4a34a375f3746a3ee6b2c
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-04-13 10:07:46 +0200

    moving out

--- original/actionRemoteCollectEnergy.js
+++ original/actionRemoteCollectEnergy.js
@@ -8,67 +8,27 @@ module.exports = function(creep){
   }
 
   if(creep.memory.collectingEnergy) {
+
+    var focusObject = Game.getObjectById(creep.memory.focus);
+    if(creep.pos.roomName != focusObject.pos.roomName){
+      creep.say("Moving out!");
+      creep.moveTo(focusObject);
+      return true;
+    }
+
     var home = Game.getObjectById(creep.memory.home);
-    if(home.memory.state != "SaveEnergy") {
-      var collectionPointsHere = creep.pos.findInRange(FIND_MY_STRUCTURES,1,{
-        filter: function(structure){
-          return structure.energy > 0;
-        }
-      });
-      if(collectionPointsHere && collectionPointsHere[0]){
-        collectionPointsHere[0].transferEnergy(creep);
+
+    var localEnergyStorage = creep.pos.findClosestByRange(FIND_STRUCTURES,{filter : function(structure){
+      if(structure && structure.store && structure.store[RESOURCE_ENERGY] && structure.store[RESOURCE_ENERGY] > 0 ){
         return true;
       }
+      return false;
+    }});
 
-      var energyCarriersHere = creep.pos.findInRange(FIND_MY_CREEPS,1,{
-        filter: function(harvestCreep){
-          if (harvestCreep.memory.role == "harvestTruck" && harvestCreep.carry.energy > 0 ) {
-            return true;
-          };
-          if (harvestCreep.memory.role == "harvester" && harvestCreep.carry.energy > 0 ) {
-            return true;
-          };
-        }
-      });
-
-      if(energyCarriersHere && energyCarriersHere[0])
-      {
-        var bestCarrier = energyCarriersHere[0]
-        for(var energyCarrierKey in energyCarriersHere){
-          var currentCarrier = energyCarriersHere[energyCarrierKey];
-          if(currentCarrier.carry.energy > bestCarrier.carry.energy){
-            bestCarrier = currentCarrier;
-          }
-        }
-        console.log(creep.name + " is getting some energy from " + bestCarrier.name);
-        bestCarrier.transferEnergy(creep);
-        return true;
-      }
-
-      var localEnergyStorage = creep.pos.findClosestByRange(FIND_STRUCTURES,{filter : function(structure){
-        if(structure && structure.store && structure.store[RESOURCE_ENERGY] && structure.store[RESOURCE_ENERGY] > 0 ){
-          return true;
-        }
-        return false;
-      }});
-
-      if(localEnergyStorage){
-        creep.say("I want energy");
-        creep.moveTo(localEnergyStorage);
-        localEnergyStorage.transfer(creep,RESOURCE_ENERGY);
-        return true;
-      }
-
-      creep.moveTo(home);
-      home.transferEnergy(creep);
+    if(localEnergyStorage){
+      creep.moveTo(localEnergyStorage);
+      localEnergyStorage.transfer(creep,RESOURCE_ENERGY);
       return true;
     }
-    else{
-      if(creep.pos.getRangeTo(home) > 5){
-        creep.moveTo(home);
-      }
-      return true;
-    }
-
   }
 }
