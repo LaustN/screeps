5823fd732b433754933c0034dc6ef7f21b6f98e7
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-04-14 10:04:09 +0200

    ignoring spawns for theft, since that does not seem to work

--- original/actionRemoteCollectEnergy.js
+++ original/actionRemoteCollectEnergy.js
@@ -40,7 +40,7 @@ module.exports = function(creep){
       }
 
       localEnergyStorage = creep.pos.findClosestByRange(FIND_STRUCTURES,{filter : function(structure){
-        if(structure && structure.energy > 0 && structure.structureType != "spawn"){
+        if(structure && structure.energy > 0 && structure.structureType && structure.structureType != "spawn"){
           return true;
         }
         return false;
