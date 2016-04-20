a4acba94700e0849907a9f1811799e63d56bed19
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-04-20 10:06:58 +0200

    making remoteTrucks scavenge

--- original/remoteTruck.js
+++ original/remoteTruck.js
@@ -9,6 +9,8 @@ module.exports = function (creep) {
         return;
     if(actionRenew(creep))
         return;
+    if(actionScavenge(creep))
+        return;
     if(actionRemoteCollectEnergy(creep))
         return;
     if(actionHomeUnloadEnergy(creep))
