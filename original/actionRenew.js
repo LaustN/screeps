8c151f907ef817bd7d7ba43c045f7a9579ed27d3
LaustN <laustn@gmail.com>
2016-04-23 23:02:30 +0200

    renewing creeps might now move out if not actually renewed

--- original/actionRenew.js
+++ original/actionRenew.js
@@ -1,32 +1,39 @@
 module.exports = function(creep){
-    var spawn = Game.getObjectById(creep.memory.home);
-    var lifetimeThatTriggersRenew = 100;
-    if(creep.pos.roomName != spawn.pos.roomName){
-      lifetimeThatTriggersRenew = 200;
+  var spawn = Game.getObjectById(creep.memory.home);
+  var lifetimeThatTriggersRenew = 100;
+  if(creep.pos.roomName != spawn.pos.roomName){
+    lifetimeThatTriggersRenew = 200;
+  }
+  if(creep.ticksToLive < lifetimeThatTriggersRenew || creep.memory.renewing){
+    creep.memory.renewing = true;
+    var renewMessage = spawn.renewCreep(creep);
+    if( renewMessage == OK){
+      if(creep.memory.lastTicksToLive){
+        if(creep.ticksToLive < creep.memory.lastTicksToLive){
+          //flee because we did not actually get renewed
+          creep.memory.renewing = false;
+        }
+      }
+      creep.memory.lastTicksToLive = creep.ticksToLive;
+      return true;
     }
-    if(creep.ticksToLive < lifetimeThatTriggersRenew || creep.memory.renewing){
-        creep.memory.renewing = true;
-        var renewMessage = spawn.renewCreep(creep);
-        if( renewMessage == OK){
-            return true;
-        }
-        if( renewMessage == ERR_NOT_IN_RANGE){
-            creep.moveTo(spawn);
-            return true;
-        }
-        else if(renewMessage == ERR_FULL){
-            console.log(creep.name + " has been renewed");
-            creep.memory.renewing = false;
-        }
-        else if(renewMessage == ERR_NOT_ENOUGH_RESOURCES){
-            console.log(creep.name + " stopped renewing because spawn did not have energy for it");
-            creep.memory.renewing = false;
-        }
-        else{
-            return false;
-            console.log("renewing did something unexpected:" + renewMessage);
-        }
+    if( renewMessage == ERR_NOT_IN_RANGE){
+        creep.moveTo(spawn);
+        return true;
+    }
+    else if(renewMessage == ERR_FULL){
+        console.log(creep.name + " has been renewed");
+        creep.memory.renewing = false;
+    }
+    else if(renewMessage == ERR_NOT_ENOUGH_RESOURCES){
+        console.log(creep.name + " stopped renewing because spawn did not have energy for it");
+        creep.memory.renewing = false;
+    }
+    else{
+        return false;
+        console.log("renewing did something unexpected:" + renewMessage);
+    }
 
-    }
-    return false;
+  }
+  return false;
 }
