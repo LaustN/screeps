0dafdd72699fc1ee6f7c849a4490304ece4cb8fc
LaustN <laustn@gmail.com>
2016-04-23 20:22:05 +0200

    no message

--- original/actionAssaultMove.js
+++ original/actionAssaultMove.js
@@ -2,8 +2,10 @@ module.exports = function(creep){
   if(creep.memory.assault){
     var targetFlag = Game.flags[creep.memory.assault];
     if(targetFlag && targetFlag.pos){
-      creep.moveTo(targetFlag);
-      return true;
+      if(targetFlag.pos.roomName != creep.pos.roomName){
+        creep.moveTo(targetFlag);
+        return true;
+      }
     }
   }
 }
