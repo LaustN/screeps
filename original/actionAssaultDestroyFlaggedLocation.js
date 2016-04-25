4abc9c65af3bae6402c6224acff4a01d13e25e19
LaustN <laustn@gmail.com>
2016-04-25 20:29:20 +0200

    tell me what is being killed

--- original/actionAssaultDestroyFlaggedLocation.js
+++ original/actionAssaultDestroyFlaggedLocation.js
@@ -2,7 +2,7 @@ module.exports = function(creep){
   if(creep.memory.assault){
     var targetFlag = Game.flags[creep.memory.assault];
     if(targetFlag && targetFlag.pos){
-      var targetStructure = creep.pos.lookFor(FIND_STRUCTURES,{filter:function(structure){
+      var targetStructure = creep.pos.findInRange(FIND_STRUCTURES,0,{filter:function(structure){
         if(structure.my){
           return false;
         }
