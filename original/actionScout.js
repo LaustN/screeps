79dd82048f3a1c152b77884de1db108dced7fa91
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-04-27 09:38:03 +0200

    not scouting while dropping off

--- original/actionScout.js
+++ original/actionScout.js
@@ -1,7 +1,7 @@
 module.exports = function(creep){
   if(creep.memory.focus){
-    if (creep.memory.dropoff) {
-      return false;      
+    if (creep.carry.energy == creep.carryCapacity) {
+      return false;  //do not scout on a full stomach
     }
     var scoutTarget = Game.getObjectById(creep.memory.focus);
     if(scoutTarget){
