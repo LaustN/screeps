f4b24ea1360f442012262088a50dbf9f7bc89967
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-04-28 10:36:27 +0200

    more aggressively razing

--- original/actionRaze.js
+++ original/actionRaze.js
@@ -1,43 +1,34 @@
 module.exports = function(creep){
   if(creep.memory.razeTarget){
-    if(creep.carryCapacity > 0 && _.sum(creep.carry)  == creep.carryCapacity){
-      creep.memory.razing = false; //do not raze on a full stomach
-    }
-    if(creep.carryCapacity > 0 && _.sum(creep.carry)  == 0){
-      creep.memory.razing = true; //ready to raze
-    }
-
-    if(creep.memory.razing){
-      var razeFlag = Game.flags[creep.memory.razeTarget];
-      if(razeFlag != null){
-        var razeRange = 0;
-        if(creep.memory.razeRange){
-          razeRange = creep.memory.razeRange;
-        }
-        else {
-          creep.memory.razeRange = 0;
-        }
-        var razeTarget = razeFlag.pos.findClosestByRange(FIND_STRUCTURES, {filter: function(structure){
-          if (structure.my) {
-            return false;
-          }
-          if (razeFlag.pos.getRangeTo(structure) <=razeRange) {
-            return true;
-          }
+    var razeFlag = Game.flags[creep.memory.razeTarget];
+    if(razeFlag != null){
+      var razeRange = 0;
+      if(creep.memory.razeRange){
+        razeRange = creep.memory.razeRange;
+      }
+      else {
+        creep.memory.razeRange = 0;
+      }
+      var razeTarget = razeFlag.pos.findClosestByRange(FIND_STRUCTURES, {filter: function(structure){
+        if (structure.my) {
           return false;
-        }});
-        if(razeTarget){
-          creep.moveTo(razeTarget);
-          creep.dismantle(razeTarget);
-          creep.attack(razeTarget);
-          if(creep.carry.energy == creep.carryCapacity){
-            creep.drop(RESOURCE_ENERGY); //while breaking stuff, do not return anything. Let others do that!
-          }
+        }
+        if (razeFlag.pos.getRangeTo(structure) <=razeRange) {
           return true;
         }
-        creep.memory.razing = false;
-        return false; //I wanted to raze, but found nothing to raze
+        return false;
+      }});
+      if(razeTarget){
+        creep.moveTo(razeTarget);
+        creep.dismantle(razeTarget);
+        creep.attack(razeTarget);
+        if(creep.carry.energy == creep.carryCapacity){
+          creep.drop(RESOURCE_ENERGY); //while breaking stuff, do not return anything. Let others do that!
+        }
+        return true;
       }
+      creep.memory.razing = false;
+      return false; //I wanted to raze, but found nothing to raze
     }
   }
   else {
