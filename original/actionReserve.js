2babe7f9a515c8a40233a41731a86effb813bbd5
LaustN <laustn@gmail.com>
2016-04-30 21:46:12 +0200

    better reserver code

--- original/actionReserve.js
+++ original/actionReserve.js
@@ -1,5 +1,9 @@
 module.exports = function(creep){
-  if(creep.reserveController(creep.room.controller)!= OK){
+  var reservationFlag = Game.flags[creep.memory.focus];
+  if(creep.room.roomName != reservationFlag.name ){
+    creep.moveTo(reservationFlag);
+  }
+  else if(creep.reserveController(creep.room.controller)!= OK){
     creep.moveTo(creep.room.controller);
   }
 }
--- original/reserver.js
+++ original/reserver.js
@@ -1,9 +1,6 @@
 module.exports = function (creep) {
-  var actionScout = require("actionScout");
   var actionReserve = require("actionReserve");
 
-  if(actionScout(creep))
-    return;
   if(actionReserve(creep))
     return;
 }
