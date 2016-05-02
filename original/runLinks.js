bd021c7681d93c9735320c428f5719e8af217cae
LaustN <laustn@gmail.com>
2016-05-02 22:01:28 +0200

    link sketchily done

--- original/runLinks.js
+++ original/runLinks.js
@@ -46,6 +46,7 @@ module.exports = function () {
     console.log("outerLink:" + outerLinks.length);
     console.log("outerLink[0]:" + JSON.stringify(outerLinks[0]));
     if (outerLinks.length > 0) {
+      console.log("transfer " + centerLink.pos + " to " + outerLinks[0].pos );
       console.log("transferMessage:" + outerLinks[0].transferEnergy(centerLink, Math.min( outerLinks[0].energy, centerLink.energyCapacity - centerLink.energy)));
       Memory.workingLinks[outerLinks[0].id] = true;
     }
