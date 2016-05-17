684aab027c0de7a290c988a49e410a6da6e2cb51
Laust Skat Nielsen <eklsn@dkspil.lokal>
2016-05-17 13:44:38 +0200

    redistributor scavenges at higher priority

--- original/creepManager.js
+++ original/creepManager.js
@@ -140,11 +140,11 @@ module.exports = function (spawn) {
     }
   }
 
-  if(fnCreateCreep(roomName + "TinyRedistributor", [CARRY,CARRY,MOVE,MOVE], {role: "redistributor", scavengeRange: 3})){
+  if(fnCreateCreep(roomName + "TinyRedistributor", [CARRY,CARRY,MOVE,MOVE], {role: "redistributor", scavengeRange: 50})){
     return;
   }
 
-  if(fnCreateCreep(roomName + "Refiller", [CARRY,CARRY,MOVE,MOVE], {role: "refiller", scavengeRange: 50 })){
+  if(fnCreateCreep(roomName + "Refiller", [CARRY,CARRY,MOVE,MOVE], {role: "refiller", scavengeRange: 3 })){
     return;
   }
 
