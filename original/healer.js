8595000819961dd95e8a45f8e4a45557dcfc30dc
Laust Skat Nielsen <eklsn@Danskespil.dk>
2016-05-24 13:14:27 +0200

    better healer

--- original/healer.js
+++ original/healer.js
@@ -1,7 +1,7 @@
 module.exports = function (creep) {
 //  var actionRenew =  require("actionRenew");
   var actionHealCreeps = require("actionHealCreeps");
-  var actionAssaultStructures = require("actionAssaultStructures");
+  var actionAssaultMove = require("actionAssaultMove");
 
   if(actionHealCreeps(creep))
     return;
