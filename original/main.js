fe73cfb87480435a350dbc75897eca802838dcbb
Laust Skat Nielsen <eklsn@Danskespil.dk>
2016-06-02 13:54:42 +0200

    less noise

--- original/main.js
+++ original/main.js
@@ -112,7 +112,6 @@ module.exports.loop = function () {
         if (action) {
           var actionResult = action(creep);
           if(actionResult){
-            console.log(creep.name + " terminated on " + actionsToTake[actionName]);
             continue creepLoop;
           }
         }
