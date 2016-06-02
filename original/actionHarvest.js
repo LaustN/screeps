62eb8f9c25b20a12e0bd86d2625a9f0b51ee46c7
Laust Skat Nielsen <eklsn@Danskespil.dk>
2016-06-02 12:14:03 +0200

    hopefully less costly harvesting code

--- original/actionHarvest.js
+++ original/actionHarvest.js
@@ -9,40 +9,31 @@ module.exports = function(creep){
 
   if(creep.memory.harvest == true){
 
-    var harvestTarget = creep.pos.findClosestByRange(FIND_SOURCES);
+    var harvestTarget = null;
     if(creep.memory.focus != null){
       var focus = Game.getObjectById(creep.memory.focus);
       if(focus != null && focus.pos.roomName == harvestTarget.pos.roomName){
         harvestTarget = focus;
       }
     }
+    if(!harvestTarget){
+      harvestTarget = creep.pos.findClosestByRange(FIND_SOURCES);
+    }
 
     var findOtherSource = function(){
-      var alternativeSource =  creep.pos.findClosestByRange(FIND_SOURCES,{ filter:
-        function(source){
-          if (source.id == harvestTarget.id) {
-            return false; //do not accept the source currently in focus
-          }
-          if(source.energy == 0){
-            return false; //do not accept empty sources
-          }
-
-          for (var xIterator = -1; xIterator <= 1; xIterator++) {
-            for (var yIterator = -1; yIterator <= 1; yIterator++) {
-              var checkPos = new RoomPosition(source.pos.x + xIterator, source.pos.y + yIterator, source.pos.roomName);
-              var terrain = checkPos.lookFor("terrain");
-              if(terrain == "plain" || terrain == "swamp"){
-                var creepsAtCheckPos = checkPos.lookFor("creep");
-                if(creepsAtCheckPos.length == 0){
-                  return true;
-                }
-              }
-            }
-          }
-          return source.id != harvestTarget.id ;
-        }});
-      if(alternativeSource != null) {
-        creep.memory.focus = alternativeSource.id;
+      var nonFocusSources = creep.room.find(FIND_SOURCES, { filter: function(source){ return source.id != creep.memory.focus && source.energy != 0}});
+      var nearbyCreepCount = 10;
+      var bestSource = null;
+      for (var sourceKey in nonFocusSources) {
+        var source = nonFocusSources[sourceKey];
+        var nearbyCreeps = source.pos.findInRange(FIND_MY_CREEPS, 1, {filter: function(nearbyCreep){ return nearbyCreep.getActiveBodyparts(WORK) > 0; } })
+        if(nearbyCreeps.length < nearbyCreepCount){
+          nearbyCreepCount = nearbyCreeps.length;
+          bestSource = source;
+        }
+      }
+      if(bestSource){
+        creep.memory.focus = bestSource.id;
       }
     }
 
