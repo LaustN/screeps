77d929ee7bebb9b4078fe3d76c2434d90b795b5d
Laust Skat Nielsen <eklsn@Danskespil.dk>
2016-06-08 10:12:46 +0200

    more bravery

--- original/actionFlee.js
+++ original/actionFlee.js
@@ -2,7 +2,7 @@ module.exports = function(creep){
   var hostilesTooClose = creep.pos.findInRange(FIND_HOSTILE_CREEPS,3);
   var hostilesClose = creep.pos.findInRange(FIND_HOSTILE_CREEPS,5);
 
-  if(hostilesTooClose){
+  if(hostilesTooClose && hostilesTooClose.length > 0){
     var destination = Game.getObjectById(creep.memory.home);
     if(creep.pos.getRangeTo(destination) <= 2){
         return false;
@@ -11,7 +11,7 @@ module.exports = function(creep){
     creep.say("flee");
     return true;
   }
-  if(hostilesClose){
+  if(hostilesClose && hostilesClose.length > 0){
     creep.moveTo(creep);
     creep.say("hold");
     return true; //just stop where you are, when the enemies are outside actual striking range
