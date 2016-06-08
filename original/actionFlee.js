b3b8aa9e37a9bda53cf7ea375277fb807bcbe88f
Laust Skat Nielsen <eklsn@Danskespil.dk>
2016-06-08 10:11:13 +0200

    flee augmented with hold

--- original/actionFlee.js
+++ original/actionFlee.js
@@ -1,15 +1,19 @@
 module.exports = function(creep){
-  var hostiles = creep.pos.findInRange(FIND_HOSTILE_CREEPS,3);
+  var hostilesTooClose = creep.pos.findInRange(FIND_HOSTILE_CREEPS,3);
+  var hostilesClose = creep.pos.findInRange(FIND_HOSTILE_CREEPS,5);
 
-  var flee = hostiles.length > 0;
-  if(flee){
-    var ensurehome = require("actionEnsureHome");
-    ensurehome(creep);
+  if(hostilesTooClose){
     var destination = Game.getObjectById(creep.memory.home);
     if(creep.pos.getRangeTo(destination) <= 2){
         return false;
     }
     creep.moveTo(destination);
+    creep.say("flee");
     return true;
+  }
+  if(hostilesClose){
+    creep.moveTo(creep);
+    creep.say("hold");
+    return true; //just stop where you are, when the enemies are outside actual striking range
   }
 }
