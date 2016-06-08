8e977d48b843ba05fe85c0df0d31d9b960d55805
Laust Skat Nielsen <eklsn@Danskespil.dk>
2016-06-08 09:46:34 +0200

    scavenge more after first pickup, if anything is found

--- original/actionScavenge.js
+++ original/actionScavenge.js
@@ -1,19 +1,28 @@
 module.exports = function(creep){
-  if(creep.carry[RESOURCE_ENERGY] > 0){
+
+
+  if(creep.carry[RESOURCE_ENERGY] == creep.carryCapacity){
+    creep.memory.scavenging = false;
+  }
+  if(creep.carry[RESOURCE_ENERGY] == 0){
+    creep.memory.scavenging = true;
+  }
+
+  if(creep.memory.scavenging){
+    var scavengeRange = 1;
+    if(creep.memory.scavengeRange){
+      scavengeRange = creep.memory.scavengeRange;
+    }
+
+    var target = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
+    if(target && target.pos.getRangeTo(creep) <= scavengeRange) {
+        if(creep.pickup(target) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(target);
+        }
+        creep.say("S" + (target.pos.x - creep.pos.x) + ";" + (target.pos.y - creep.pos.y));
+        return true;
+    }
+    creep.memory.scavenging = false;
     return false;
-  }
-
-  var scavengeRange = 1;
-  if(creep.memory.scavengeRange){
-    scavengeRange = creep.memory.scavengeRange;
-  }
-
-  var target = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
-  if(target && target.pos.getRangeTo(creep) <= scavengeRange) {
-      if(creep.pickup(target) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(target);
-      }
-      creep.say("S" + (target.pos.x - creep.pos.x) + ";" + (target.pos.y - creep.pos.y));
-      return true;
   }
 }
