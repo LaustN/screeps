b3ce813f42105f019c30a53685473ccabd40bd9d
Laust Skat Nielsen <eklsn@Danskespil.dk>
2016-06-13 09:59:00 +0200

    drop the hold state, since it causes wall repairers to panic when under siege

--- original/actionFlee.js
+++ original/actionFlee.js
@@ -1,6 +1,5 @@
 module.exports = function(creep){
   var hostilesTooClose = creep.pos.findInRange(FIND_HOSTILE_CREEPS,3);
-  var hostilesClose = creep.pos.findInRange(FIND_HOSTILE_CREEPS,4);
 
   if(hostilesTooClose && hostilesTooClose.length > 0){
     var destination = Game.getObjectById(creep.memory.home);
@@ -10,10 +9,5 @@ module.exports = function(creep){
     creep.moveTo(destination);
     creep.say("flee");
     return true;
-  }
-  if(hostilesClose && hostilesClose.length > 0){
-    creep.moveTo(creep);
-    creep.say("hold");
-    return true; //just stop where you are, when the enemies are outside actual striking range
   }
 }
