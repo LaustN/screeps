c7f02580d4b97405e55fe78dd5e87ed3ee1aa0b5
lsn <laustn@gmail.com>
2017-08-07 10:15:44 +0200

    no longer holding

--- original/actionHold.js
+++ original/actionHold.js
@@ -1,12 +1,15 @@
 module.exports = function(creep){
+  creep.say("cosider holding");
   if (!creep.memory.holdDuration || creep.memory.holdDuration<100) {
 
     if(creep.memory.lastHoldTick == Game.time-1){
       creep.memory.lastHoldTick = Game.time;
       creep.memory.holdDuration ++;
+      creep.say("holding");
       return true;
     }
     else{
+      creep.say("no longer holding");
       creep.memory.holdDuration = 0;
       return false;
     }
