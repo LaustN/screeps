e248422054a94c78531695f63554a9afe8a69687
lsn <laustn@gmail.com>
2017-08-07 10:54:48 +0200

    parenthesis in boolean statement might help

--- original/actionHold.js
+++ original/actionHold.js
@@ -1,20 +1,20 @@
 module.exports = function(creep){
-  creep.say("cosider holding");
+  console.log("cosider holding");
   if (typeof(creep.memory.holdDuration) == "undefined" || creep.memory.holdDuration<500) {
 
     if (typeof(creep.memory.lastHoldTick) == "undefined"){
       creep.memory.lastHoldTick = Game.time;
-      creep.say("initial hold time");
+      console.log("initial hold time");
     }
 
-    if(creep.memory.lastHoldTick >= Game.time-1){
+    if(creep.memory.lastHoldTick >= (Game.time-1)){
       creep.memory.lastHoldTick = Game.time;
       creep.memory.holdDuration ++;
-      creep.say("holding");
+      console.log("holding");
       return true;
     }
     else{
-      creep.say("no longer holding");
+      console.log("no longer holding");
       creep.memory.holdDuration = 0;
       return false;
     }
