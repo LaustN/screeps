21887b58feeeb7fe0e711cd3c943a75f3c94c4b7
lsn <laustn@gmail.com>
2017-08-10 15:32:05 +0200

    mild og konstruktiv!

--- original/roomBuildings.js
+++ original/roomBuildings.js
@@ -1,15 +1,11 @@
 module.exports = function (room) {
-    console.log("spam!")
     room.visual.clear();
     var spawns  = room.find(FIND_MY_STRUCTURES, {filter: function(structure){
-        console.log("the structure is " + JSON.stringify(structure));
         if(structure.structureType == STRUCTURE_SPAWN)
             return true;
         return false;
     }});
     for(var spawnkey in spawns) {
-        console.log("spawnkey = " + spawnkey);
         var spawn  = spawns[spawnkey];
-        room.visual.text(spawn.name + " obviously", spawn.pos);
     }
 }
