9a25b3c0eba08c04f07a543f9d76691871ee4e45
lsn <laustn@gmail.com>
2017-08-28 12:12:03 +0200

    assign like you mean it!

--- rework2017/actionDistributeToWorkers.js
+++ rework2017/actionDistributeToWorkers.js
@@ -1,8 +1,8 @@
 module.exports = function (creep) {
   if (creep.carry[RESOURCE_ENERGY] > 0) {
     var target = Game.getObjectById(creep.memory.focus);
-    if (!target || (!target.memory) || (typeof(target.memory.energyWanted) == "undefined") || !(target.memory.energyWanted > 0) ) {
-      target == null;
+    if (!target || (!target.memory) || !(target.memory.energyWanted > 0)) {
+      target = null;
     }
 
     if (!target) {
