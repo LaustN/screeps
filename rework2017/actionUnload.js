48e38083ee5c9a977b09ca805df77fdd1084dce5
lsn <laustn@gmail.com>
2017-08-28 09:49:49 +0200

    more robust energy unload

--- rework2017/actionUnload.js
+++ rework2017/actionUnload.js
@@ -11,21 +11,23 @@ module.exports = function (creep) {
 
   if (creep.memory.unloading) {
     var target = Game.getObjectById(creep.memory.focus);
-    switch (target.structureType) {
-      case STRUCTURE_EXTENSION:
-        if (target.energy == target.energyCapacity)
-          target = null;
-        break;
-      case STRUCTURE_SPAWN:
-        if (target.energy == target.energyCapacity)
-          target = null;
-        break;
-      case STRUCTURE_STORAGE:
-        //do nothing, assume that storage has capacity
-        break;
-      default:
-        target = null; //ocus must be legacy, so null it
-        break;
+    if (target) {
+      switch (target.structureType) {
+        case STRUCTURE_EXTENSION:
+          if (target.energy == target.energyCapacity)
+            target = null;
+          break;
+        case STRUCTURE_SPAWN:
+          if (target.energy == target.energyCapacity)
+            target = null;
+          break;
+        case STRUCTURE_STORAGE:
+          //do nothing, assume that storage has capacity
+          break;
+        default:
+          target = null; //ocus must be legacy, so null it
+          break;
+      }
     }
     if (!target) {
       if (creep.room.storage) {
@@ -33,11 +35,11 @@ module.exports = function (creep) {
       }
     }
     if (!target) {
-      target = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_SPAWN }});
+      target = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_SPAWN } });
     }
-    if(target){
+    if (target) {
       var transferMessage = creep.transfer(target, RESOURCE_ENERGY);
-      if(transferMessage == ERR_NOT_IN_RANGE)
+      if (transferMessage == ERR_NOT_IN_RANGE)
         creep.moveTo(target);
       return true;
     }
