cb534473d47ce5e010b907d8e6ab9d348a4d7cc3
lsn <laustn@gmail.com>
2017-08-31 16:06:32 +0200

    simple upgrade implemented

--- rework2017/actionUpgradeController.js
+++ rework2017/actionUpgradeController.js
@@ -1,5 +1,10 @@
 module.exports = function(creep){
-  console.log("actionUpgradeController not implemented");
+  if(creep.room.controller && creep.room.controller.my){
+    var upgradeMessage = creep.upgradeController(creep.room.controller);
+    if(upgradeMessage == ERR_NOT_IN_RANGE){
+      creep.moveTo(creep.room.controller);
+    }
+  }
   return false;
 }
   
