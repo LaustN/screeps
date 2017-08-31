dfea452ab531e68628f867b2c0d889ab2cbc743a
lsn <laustn@gmail.com>
2017-08-31 12:58:54 +0200

    repair implemented sketchily

--- rework2017/actionRepair.js
+++ rework2017/actionRepair.js
@@ -1,5 +1,35 @@
-module.exports = function(creep){
-  console.log("actionRepair not implemented");
+module.exports = function (creep) {
+  if (creep.carry[RESOURCE_ENERGY] > 0) {
+    var target = null;
+    if (creep.memory.focus) {
+      var existingTarget = Game.getObjectById(creep.memory.focus);
+      if (existingTarget && (existingTarget.hits < existingTarget.hitsMax)) {
+        target = existingTarget;
+      }
+    }
+    if (!target) {
+      target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: function (structure) {
+          if (
+            (structure.structureType != STRUCTURE_WALL)
+            && (structure.structureType != STRUCTURE_RAMPART)
+            && (structure.hits < structure.hitsMax)
+          ) {
+            return true;
+          }
+          return false;
+        }
+      });
+    }
+    if (target) {
+      creep.memory.focus = target.id;
+      if (target.pos.getRangeTo(creep) <= 3) {
+        creep.repair(target);
+      } else {
+        creep.moveTo(target);
+      }
+      return true;
+    }
+  }
   return false;
 }
-  
