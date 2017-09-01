15af6d6546d738b454d72e6f7191a9fa1e14a7c2
lsn <laustn@gmail.com>
2017-09-01 11:05:21 +0200

    implementing fortifu

--- rework2017/actionFortify.js
+++ rework2017/actionFortify.js
@@ -1,5 +1,54 @@
-module.exports = function(creep){
+module.exports = function (creep) {
+  //figure out how high walls need to be
+  var desiredHitsPerWall = creep.room.controller.level * 500;
+  if (creep.room.controller.level > 7) {
+    desiredHitsPerWall = 300000000;
+  }
+  var target = Game.getObjectById(creep.memory.focus);
+  if (target) {
+    if (target.structureType) {
+      if ((target.structureType != STRUCTURE_RAMPART) && (target.structureType != STRUCTURE_RAMPART)) {
+        target = null;
+      }
+    }
+    else {
+      target = null;
+    }
+  }
+
+  if (!target) {
+    target = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+      filter: function (structure) {
+        if (structure.structureType != STRUCTURE_RAMPART)
+          return false;
+        if (structure.hits < desiredHitsPerWall)
+          return true;
+        return false;
+      }
+    });
+  }
+  if (!target) {
+    target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+      filter: function (structure) {
+        if (structure.structureType != STRUCTURE_WALL)
+          return false;
+        if (structure.hits < desiredHitsPerWall)
+          return true;
+        return false;
+      }
+    });
+  }
+  if (target) {
+    creep.memory.focus = target.id;
+    var repairMessage = creep.repair(target);
+    if (repairMessage == ERR_NOT_IN_RANGE) {
+      creep.moveTo(target);
+    }
+  }
+
+
+  //find a rampart that needs strengthening
+  // else find wall that needs strengthening
   console.log("actionFortify not implemented yet");
   return false;
 }
-  
