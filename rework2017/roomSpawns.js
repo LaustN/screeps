e4cde589854bccb9406272f0485893fa9c0e931e
LaustN <laustn@gmail.com>
2017-09-04 00:04:35 +0200

    make defenders get roles

--- rework2017/roomSpawns.js
+++ rework2017/roomSpawns.js
@@ -12,7 +12,8 @@ module.exports = function (room) {
       }
       // room.memory.spawnQueue.push({ body: workerBody, type: "work", name: workerName });
       var spawnOrder = room.memory.spawnQueue[0];
-      var spawnMessage = spawn.createCreep(spawnOrder.body, spawnOrder.name, { type: spawnOrder.type });
+
+      var spawnMessage = spawn.createCreep(spawnOrder.body, spawnOrder.name, { type: spawnOrder.type, role: spawnOrder.role });
       if (spawnMessage == spawnOrder.name) {
         room.memory.spawnQueue = _.drop(room.memory.spawnQueue, 1);
         continue;
