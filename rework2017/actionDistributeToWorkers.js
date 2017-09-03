445bb02a698a6271b0ce2ad3d33363bf695d7020
LaustN <laustn@gmail.com>
2017-09-03 23:24:31 +0200

    avoid overfeeding

--- rework2017/actionDistributeToWorkers.js
+++ rework2017/actionDistributeToWorkers.js
@@ -10,6 +10,20 @@ module.exports = function (creep) {
       }
     }
 
+    if (target) {
+      var feeders = target.pos.findInRange(FIND_MY_CREEPS, 2, {
+        filter: function (feeder) {
+          return (feeder.memory.energyWanted < 0)
+            && ((feeder.carry[RESOURCE_ENERGY] / feeder.carryCapacity) > 0.5)
+            && feeder.id != creep.id; //do not count yourself as an extra feeded
+        }
+      });
+
+      if (feeders.length > 0) {
+        target = null;
+      }
+    }
+
     if (!target) {
       var hungryCreep = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
         filter: function (filterCreep) {
@@ -30,14 +44,16 @@ module.exports = function (creep) {
       }
     }
 
-    if(target && (target.carry[RESOURCE_ENERGY] /target.carryCapacity) > 0.5 ){
+    if (target && (target.carry[RESOURCE_ENERGY] / target.carryCapacity) > 0.5) {
       //target is more than half full, consider secondary targets
-      var secondaries = target.pos.findInRange(FIND_MY_CREEPS, 2, {filter: function(secondary){
-        return (secondary.memory.energyWanted > 0)
-        && ((secondary.carry[RESOURCE_ENERGY] / secondary.carryCapacity) < 0.5)
-      }});
+      var secondaries = target.pos.findInRange(FIND_MY_CREEPS, 2, {
+        filter: function (secondary) {
+          return (secondary.memory.energyWanted > 0)
+            && ((secondary.carry[RESOURCE_ENERGY] / secondary.carryCapacity) < 0.5)
+        }
+      });
 
-      if(secondaries.length){
+      if (secondaries.length) {
         target = secondaries[0];
       }
     }
