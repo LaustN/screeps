2a6d86f41cef7e6a97f775f6c3c54e17bb2c58db
LaustN <laustn@gmail.com>
2017-09-03 22:03:52 +0200

    do not concern yourself as a repairer about having energy

--- rework2017/actionRepair.js
+++ rework2017/actionRepair.js
@@ -1,39 +1,37 @@
 module.exports = function (creep) {
   console.log("repair was called for " + creep.name);
-  if (creep.carry[RESOURCE_ENERGY] > 0) {
-    var target = null;
-    if (creep.memory.focus) {
-      var existingTarget = Game.getObjectById(creep.memory.focus);
-      if (existingTarget && (existingTarget.hits < existingTarget.hitsMax)) {
-        target = existingTarget;
-        console.log("reusing repair target");
-      }
+  var target = null;
+  if (creep.memory.focus) {
+    var existingTarget = Game.getObjectById(creep.memory.focus);
+    if (existingTarget && (existingTarget.hits < existingTarget.hitsMax)) {
+      target = existingTarget;
+      console.log("reusing repair target");
     }
-    if (!target) {
-      target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: function (structure) {
-          if (
-            (structure.structureType != STRUCTURE_WALL)
-            && (structure.structureType != STRUCTURE_RAMPART)
-            && (structure.hits < structure.hitsMax)
-          ) {
-            console.log( "structure at " + structure.pos + " is an ok repair target" );
-            return true;
-          }
-          console.log( "structure at " + structure.pos + " is not an ok repair target" );
-          return false;
+  }
+  if (!target) {
+    target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+      filter: function (structure) {
+        if (
+          (structure.structureType != STRUCTURE_WALL)
+          && (structure.structureType != STRUCTURE_RAMPART)
+          && (structure.hits < structure.hitsMax)
+        ) {
+          console.log("structure at " + structure.pos + " is an ok repair target");
+          return true;
         }
-      });
-    }
-    if (target) {
-      creep.memory.focus = target.id;
-      if (target.pos.getRangeTo(creep) <= 3) {
-        creep.repair(target);
-      } else {
-        creep.moveTo(target);
+        console.log("structure at " + structure.pos + " is not an ok repair target");
+        return false;
       }
-      return true;
+    });
+  }
+  if (target) {
+    creep.memory.focus = target.id;
+    if (target.pos.getRangeTo(creep) <= 3) {
+      creep.repair(target);
+    } else {
+      creep.moveTo(target);
     }
+    return true;
   }
   return false;
 }
