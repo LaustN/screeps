fd2d54ecadb20bd7b08e0eef51a9cc02ebe00ce8
LaustN <laustn@gmail.com>
2017-09-03 23:55:04 +0200

    implementing recycling

--- rework2017/actionRecycle.js
+++ rework2017/actionRecycle.js
@@ -1,5 +1,8 @@
-module.exports = function(creep){
-  console.log("actionRecycle not implemented");
-  return false;
+module.exports = function (creep) {
+  var spawn = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { structureType: STRUCTURE_SPAWN })
+  var recycleMessage = spawn.recycle(creep);
+  if (recycleMessage == ERR_NOT_IN_RANGE)
+    creep.moveTo(spawn);
+
+  return true;
 }
-  
