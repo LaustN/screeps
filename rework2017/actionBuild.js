a99193927231654c5e6b54d879e5593c3865dece
LaustN <laustn@gmail.com>
2017-09-03 22:05:50 +0200

    move to build target right away

--- rework2017/actionBuild.js
+++ rework2017/actionBuild.js
@@ -1,26 +1,26 @@
 module.exports = function (creep) {
-  if (creep.carry[RESOURCE_ENERGY] > 0) {
-    var target = null;
-    if (creep.memory.focus) {
-      var existingTarget = Game.getObjectById(creep.memory.focus);
-      if (existingTarget && existingTarget.progressTotal) {
-        target = existingTarget;
+  var target = null;
+  if (creep.memory.focus) {
+    var existingTarget = Game.getObjectById(creep.memory.focus);
+    if (existingTarget && existingTarget.progressTotal) {
+      target = existingTarget;
+    }
+  }
+  if (!target) {
+    target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES, {
+      filter: function (constructionsite) {
+        return ((constructionsite.structureType != STRUCTURE_WALL) && (constructionsite.structureType != STRUCTURE_RAMPART));
       }
+    });
+  }
+  if (target && target.progressTotal) {
+    creep.memory.focus = target.id;
+    if (target.pos.getRangeTo(creep) <= 3) {
+      creep.build(target);
+    } else {
+      creep.moveTo(target);
     }
-    if (!target) {
-      target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES,{filter: function(constructionsite){
-        return(( constructionsite.structureType != STRUCTURE_WALL) && (constructionsite.structureType != STRUCTURE_RAMPART));
-      }});
-    }
-    if (target && target.progressTotal) {
-      creep.memory.focus = target.id;
-      if (target.pos.getRangeTo(creep) <= 3) {
-        creep.build(target);
-      } else {
-        creep.moveTo(target);
-      }
-      return true;
-    }
+    return true;
   }
   return false;
 }
