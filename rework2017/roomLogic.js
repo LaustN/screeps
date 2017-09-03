83b963aa9e09b8bd8f1f18f24bab71ad1e93bf47
LaustN <laustn@gmail.com>
2017-09-03 21:09:19 +0200

    double workers per full container

--- rework2017/roomLogic.js
+++ rework2017/roomLogic.js
@@ -37,7 +37,7 @@ module.exports = function () {
 
 		var sources = room.find(FIND_SOURCES);
 
-		var workerPairsWanted = fullContainers.length + sources.length + 2;
+		var workerPairsWanted = fullContainers.length * 2 + sources.length + 2;
 
 		if ((room.energyCapacityAvailable < 550) || (workCount < 1) || (moveCount < 1)) {
 			//processing starts for frontier
