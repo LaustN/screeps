a59bf18544f228e12ff28deb0219f29caa49e5c1
LaustN <laustn@gmail.com>
2017-09-04 21:14:35 +0200

    better recycling

--- rework2017/actionRecycle.js
+++ rework2017/actionRecycle.js
@@ -1,9 +1,11 @@
 module.exports = function (creep) {
   creep.say("NO MORE!!");
-  var spawn = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { structureType: STRUCTURE_SPAWN });
-  var recycleMessage = spawn.recycleCreep(creep);
-  if (recycleMessage == ERR_NOT_IN_RANGE)
-    creep.moveTo(spawn);
+  var spawn = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_SPAWN } });
+  if (spawn) {
+    var recycleMessage = spawn.recycleCreep(creep);
+    if (recycleMessage == ERR_NOT_IN_RANGE)
+      creep.moveTo(spawn);
 
-  return true;
+    return true;
+  }
 }
