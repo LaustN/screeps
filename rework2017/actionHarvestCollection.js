018eebbaad500cacd67d461e5a4d9f325ebc7b51
lsn <laustn@gmail.com>
2017-09-04 15:56:53 +0200

    much more logging in harvest collection

--- rework2017/actionHarvestCollection.js
+++ rework2017/actionHarvestCollection.js
@@ -3,6 +3,7 @@ module.exports = function (creep) {
   var source = Game.getObjectById(creep.memory.focus);
   if (creep.pos.getRangeTo(source) > 5) {
     creep.moveTo(source);
+    console.log(creep.name + " moving to focus");
     return true;
   }
 
@@ -14,6 +15,7 @@ module.exports = function (creep) {
       //only return from scavenging if movement was needed
       creep.moveTo(droppedEnergy[0]);
       isMoving = true;
+      console.log(creep.name + " picking up dropped energy");
     }
   }
 
@@ -27,6 +29,8 @@ module.exports = function (creep) {
   if (structuresWithStorage.length > 0) {
     if (creep.withdraw(structuresWithStorage[0]) == ERR_NOT_IN_RANGE && !isMoving)
       creep.moveTo(structuresWithStorage[0]);
+    console.log(creep.name + " collecting from structure");
+    
     return true;
   }
 
@@ -37,7 +41,9 @@ module.exports = function (creep) {
       return false;
     }
   });
-  if (providingCreeps && providingCreeps.length) {
+  if (providingCreeps.length > 0) {
+    console.log(creep.name + " collecting from provider");
+    
     var transferMessage = providingCreeps[0].transfer(creep, RESOURCE_ENERGY);
     if(transferMessage == ERR_NOT_IN_RANGE && !isMoving){
       creep.moveTo(providingCreeps[0]);
