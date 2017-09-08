74f41bc73ddd2183a12127a4ee0065d01261bec1
lsn <laustn@gmail.com>
2017-09-08 11:22:20 +0200

    make upgraders move even when no energy is carried

--- spawnQueue/actionUpgradeController.js
+++ spawnQueue/actionUpgradeController.js
@@ -1,8 +1,15 @@
 module.exports = function(creep){
   if(creep.room.controller && creep.room.controller.my){
     var upgradeMessage = creep.upgradeController(creep.room.controller);
+    if(upgradeMessage == OK)
+      return true;
     if(upgradeMessage == ERR_NOT_IN_RANGE){
       creep.moveTo(creep.room.controller);
+      return true;
+    }
+    if(creep.pos.getRangeTo(creep.room.controller)>3){
+      creep.moveTo(creep.room.controller);
+      return true;
     }
   }
   return false;
