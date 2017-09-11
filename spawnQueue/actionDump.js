30fbb38975e6c9db6fd783ba4cdcbad84870487c
lsn <laustn@gmail.com>
2017-09-11 13:20:59 +0200

    try not to transfer to a road

--- spawnQueue/actionDump.js
+++ spawnQueue/actionDump.js
@@ -1,7 +1,15 @@
 module.exports = function (creep) {
   if (creep.carry[RESOURCE_ENERGY] > 0) {
     console.log(creep.name + " dumping");
-    var structuresHere = creep.pos.findInRange(FIND_STRUCTURES, 1);
+    var structuresHere = creep.pos.findInRange(FIND_STRUCTURES, 1, {
+      filter: function (structure) { //roads are also structures, so the filter is actually neeeded!
+        if (structure.structureType == STRUCTURE_CONTAINER)
+          return true;
+        if (structure.structureType == STRUCTURE_LINK)
+          return true;
+        return false;
+      }
+    });
     if (structuresHere.length > 0) {
       console.log(creep.name + " transfering to structure");
       creep.transfer(structuresHere[0], RESOURCE_ENERGY);
