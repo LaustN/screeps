82f962e5fac6fe80f725a67e622fe5596fd93bc8
lsn <laustn@gmail.com>
2017-09-11 13:18:19 +0200

    logging in dump code

--- spawnQueue/actionDump.js
+++ spawnQueue/actionDump.js
@@ -1,7 +1,9 @@
 module.exports = function (creep) {
   if (creep.carry[RESOURCE_ENERGY] > 0) {
+    console.log(creep.name + " dumping");
     var structuresHere = creep.pos.findInRange(FIND_STRUCTURES, 1);
-    if (structuresHere && structuresHere.length) {
+    if (structuresHere.length > 0) {
+      console.log(creep.name + " transfering to structure");
       creep.transfer(structuresHere[0], RESOURCE_ENERGY);
       return true;
     }
