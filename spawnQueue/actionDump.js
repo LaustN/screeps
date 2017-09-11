60ae4575eada945667cdcc838fd6634dde8de875
lsn <laustn@gmail.com>
2017-09-11 13:11:18 +0200

    only drop energy when no buildings are in range

--- spawnQueue/actionDump.js
+++ spawnQueue/actionDump.js
@@ -1,9 +1,12 @@
-module.exports = function(creep){
-  var structuresHere = creep.pos.findInRange(FIND_STRUCTURES,1);
-  if(structuresHere && structuresHere.length){
-    creep.transfer(structuresHere[0],RESOURCE_ENERGY);
+module.exports = function (creep) {
+  if (creep.carry[RESOURCE_ENERGY] > 0) {
+    var structuresHere = creep.pos.findInRange(FIND_STRUCTURES, 1);
+    if (structuresHere && structuresHere.length) {
+      creep.transfer(structuresHere[0], RESOURCE_ENERGY);
+      return true;
+    }
+    creep.drop(RESOURCE_ENERGY);
+    return true;
   }
-  creep.drop(RESOURCE_ENERGY);
-
+  return false;
 }
-  
