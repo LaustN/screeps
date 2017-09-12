d837f425a37f2b3255fce6d51f4caf6a9b29522d
LaustN <laustn@gmail.com>
2017-09-12 14:28:47 +0200

    simple text folofo was here

--- spawnQueue/actionSign.js
+++ spawnQueue/actionSign.js
@@ -1,5 +1,5 @@
 module.exports = function(creep){
-  var sign ="(╯°□°）╯︵ ┻━┻";
+  var sign ="Folofo was here!";
   if(creep.room.controller && ( (!creep.room.controller.sign) ||  (creep.room.controller.sign.text != sign))){
     if(creep.pos.isNearTo(creep.room.controller)){
       creep.signController(creep.room.controller,sign);
