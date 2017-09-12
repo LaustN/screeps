dbe8f4ae6b6a199e78f222d6bb5cecb7eb3971db
LaustN <laustn@gmail.com>
2017-09-12 14:11:48 +0200

    carefull when comparing undefined

--- spawnQueue/actionMigrate.js
+++ spawnQueue/actionMigrate.js
@@ -1,7 +1,7 @@
 module.exports = function(creep){
   var flag = Game.flags[creep.memory.flag];
   if(flag){
-    if(creep.pos.room != flag.room){
+    if(creep.pos.roomName != flag.pos.roomName){
       creep.moveTo(flag);
       return true;
     }
