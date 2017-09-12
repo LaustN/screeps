599ecffcfd4227abfb7163c2cdb670f9e82bb530
LaustN <laustn@gmail.com>
2017-09-12 14:25:26 +0200

    check for sign presence before checking sign text

--- spawnQueue/actionSign.js
+++ spawnQueue/actionSign.js
@@ -1,6 +1,6 @@
 module.exports = function(creep){
   var sign ="(╯°□°）╯︵ ┻━┻";
-  if(creep.room.controller && (creep.room.controller.sign.text != sign)){
+  if(creep.room.controller && ( (!creep.room.controller.sign) ||  (creep.room.controller.sign.text != sign))){
     if(creep.pos.isNearTo(creep.room.controller)){
       creep.signController(creep.room.controller,sign);
     }
