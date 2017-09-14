57afe9f94e27b0a28bfc0c7f683dd4d24c438163
lsn <laustn@gmail.com>
2017-09-14 17:24:55 +0200

    stupid copy-pasta errors!

--- spawnQueue/actionClaim.js
+++ spawnQueue/actionClaim.js
@@ -1,6 +1,6 @@
 module.exports = function (creep) {
   if (creep.room.controller) {
-    if (creep.pos.isNearTo(creep, room.controller))
+    if (creep.pos.isNearTo(creep.room.controller))
       creep.claimController(creep.room.controller);
     else
       creep.moveTo(creep.room.controller);
