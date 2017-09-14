2bcd11d784cfeee5469a4cfcc7a84e3b8edbcd09
lsn <laustn@gmail.com>
2017-09-14 16:22:05 +0200

    better reservation range detection

--- spawnQueue/actionReserve.js
+++ spawnQueue/actionReserve.js
@@ -1,6 +1,6 @@
 module.exports = function (creep) {
   if (creep.room.controller) {
-    if (creep.pos.isNearTo(creep, creep.room.controller))
+    if (creep.pos.isNearTo(creep.room.controller))
       creep.reserveController(creep.room.controller);
     else
       creep.moveTo(creep.room.controller);
