5fdb3fe86057fa09b6647515855c038205de1673
lsn <laustn@gmail.com>
2017-09-14 17:37:37 +0200

    only accept dumping to nofull containers

--- spawnQueue/actionDump.js
+++ spawnQueue/actionDump.js
@@ -3,8 +3,10 @@ module.exports = function (creep) {
     console.log(creep.name + " dumping");
     var structuresHere = creep.pos.findInRange(FIND_STRUCTURES, 3, {
       filter: function (structure) { //roads are also structures, so the filter is actually neeeded!
-        if (structure.structureType == STRUCTURE_CONTAINER)
-          return true;
+        if (structure.structureType == STRUCTURE_CONTAINER){
+          if(_.sum(structure.store) < structure.storeCapacity)
+            return true;
+        }
         if (structure.structureType == STRUCTURE_LINK)
           return true;
         return false;
