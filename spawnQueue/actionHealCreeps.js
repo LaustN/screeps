a9aa8a65f51459d0f5f382e1123c8ffbb199541e
LaustN <laustn@gmail.com>
2017-09-15 23:04:21 +0200

    better healer filter

--- spawnQueue/actionHealCreeps.js
+++ spawnQueue/actionHealCreeps.js
@@ -1,9 +1,9 @@
 module.exports = function(creep){
-  var wounded = creep.pos.findClosestByRange(FIND_MY_CREEPS,function(woundedCreep){
+  var wounded = creep.pos.findClosestByRange(FIND_MY_CREEPS,{filter: function(woundedCreep){
     if(woundedCreep.hits < woundedCreep.hitsMax)
       return true;
     return false;
-  });
+  }});
   if(wounded){
     console.log(creep.name + " healing " + wounded.name);
     creep.moveTo(wounded);
