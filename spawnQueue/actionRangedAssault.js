08dae6fb904911612c3a4cc98036cf0133f3e2d4
LaustN <laustn@gmail.com>
2017-09-15 22:21:15 +0200

    rangedAssault now returns better

--- spawnQueue/actionRangedAssault.js
+++ spawnQueue/actionRangedAssault.js
@@ -62,29 +62,9 @@ module.exports = function (creep) {
         if (moveResult == ERR_NO_PATH) {
           creep.room.assaultTarget = null; //for the off chance that a target flees out of range inside a room
         }
+        return true;
       }
     }
-  }
-
-
-
-  if (creep.room.memory.enemiesHere && creep.room.memory.enemiesHere.length) {
-    var target = Game.getObjectById(creep.room.memory.enemiesHere[0]);
-    var targetRange = creep.pos.getRangeTo(target);
-    if (targetRange > 3) {
-      creep.moveTo(target);
-    }
-    if (targetRange < 3) {
-      console.log(creep.name + " might move away?");
-    }
-    if (creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3).length > 1) {
-      creep.rangedMassAttack();
-    } else {
-      creep.rangedAttack(target);
-    }
-    return true;
-  } else {
-    console.log(creep.name + " did not see enemies in " + creep.room.name);
   }
   return false;
 }
