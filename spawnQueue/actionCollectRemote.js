0b68ba68f9428c13380525cd52d1ee10bb210913
lsn <laustn@gmail.com>
2017-09-15 12:01:45 +0200

    better collectremote

--- spawnQueue/actionCollectRemote.js
+++ spawnQueue/actionCollectRemote.js
@@ -1,5 +1,4 @@
 module.exports = function (creep) {
-  //Did consider finding a non-closest container, but things are going to storage rather than closest container anyhow
   var containerFilter = function (structure) {
     if ((structure.structureType == STRUCTURE_CONTAINER) && (structure.store[RESOURCE_ENERGY] > 0))
       return true;
@@ -7,7 +6,6 @@ module.exports = function (creep) {
   };
 
   if (creep.carry[RESOURCE_ENERGY] == 0) {
-
     var homeLink = null;
     if (creep.room.storage) {
       homeLink = creep.room.storage.pos.findClosestByRange(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_LINK } });
@@ -26,18 +24,18 @@ module.exports = function (creep) {
           target = creep.pos.findClosestByRange(FIND_STRUCTURES, { filter: containerFilter });
         }
       }
-
-      if (target && ((target.store && target.store[RESOURCE_ENERGY] > 0) || target.energy > 0)) {
-        creep.memory.focus = target.id;
-
-        if (target.pos.getRangeTo(creep) <= 1) {
-          creep.withdraw(target, RESOURCE_ENERGY);
-        } else {
-          creep.moveTo(target);
-        }
-        return true;
-      }
     }
+    if (target && ((target.store && target.store[RESOURCE_ENERGY] > 0) || target.energy > 0)) {
+      creep.memory.focus = target.id;
+
+      if (target.pos.getRangeTo(creep) <= 1) {
+        creep.withdraw(target, RESOURCE_ENERGY);
+      } else {
+        creep.moveTo(target);
+      }
+      return true;
+    }
+
   }
   return false;
 }
