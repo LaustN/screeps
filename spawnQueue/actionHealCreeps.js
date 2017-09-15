81d9e342687b775f5bb4a11ce2cb8dab5d85af95
LaustN <laustn@gmail.com>
2017-09-15 22:35:18 +0200

    heal implemented quickly

--- spawnQueue/actionHealCreeps.js
+++ spawnQueue/actionHealCreeps.js
@@ -1,4 +1,17 @@
 module.exports = function(creep){
-  
+  var wounded = creep.pos.findClosestByRange(FIND_MY_CREEPS,function(woudedCreep){
+    if(woundedCreep.hits < woundedCreep.hitsMax)
+      return true;
+    return false;
+  });
+  if(wounded){
+    creep.moveTo(wounded)
+    if(creep.isNearTo(wounded))
+      creep.heal(wounded);
+    else
+      creep.rangedHeal(wounded);
+    return true;
+  }
+  return false;
 }
   
