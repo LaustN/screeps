443f055f7d80c6e35455e0d68af58a872a6d11ca
LaustN <laustn@gmail.com>
2017-09-18 22:07:18 +0200

    approach target more consistently

--- spawnQueue/actionAssault.js
+++ spawnQueue/actionAssault.js
@@ -71,7 +71,7 @@ module.exports = function (creep) {
   if (target) {
     var targetRange = creep.pos.getRangeTo(target);
     console.log("target structureType is: " + target.structureType);
-    if ((targetRange > 3) || (target.structureType)) {
+    if ((targetRange > 3) || (typeof(target.structureType) != "undefined") ) {
       creep.moveTo(target);
     }
     if (targetRange < 3) {
