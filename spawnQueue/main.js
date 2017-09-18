0db3084cb544940d22abd828fba78e646924d374
lsn <laustn@gmail.com>
2017-09-18 10:06:18 +0200

    disable logging for a bit

--- spawnQueue/main.js
+++ spawnQueue/main.js
@@ -5,52 +5,53 @@ var roomLogic = require("roomLogic")
 
 
 var roleActions = {
-  "harvester": ["actionSetGivesEnergy","actionHarvest","actionDump"],
-  "harvestWithReturn" : ["actionSetGivesEnergy","actionHarvest","actionUnload"],
-  "builder": ["actionSetWantsEnergy","actionBuild"],
-  "controlUpgrader": ["actionSetWantsEnergy","actionUpgradeController"],
-  "fortifier": ["actionSetWantsEnergy","actionFortify"],
-  "repairer": ["actionSetWantsEnergy","actionRepair"],
-  
-  "harvestTruck": ["actionSetGivesEnergy","actionUnload","actionHarvestCollection"],
-  "resupplyBuildings":["actionSetMovesEnergy","actionDistributeToBuildings","actionFetchFromStorage"],
-  "stockpile":["actionSetMovesEnergy","actionCollectRemote","actionFillStorage"],
-  "resupplyWorkers":["actionSetMovesEnergy","actionDistributeToWorkers","actionFetchFromStorage"],
-  "looter" :["actionSetGivesEnergy","actionMigrate","actionFetchRemote", "actionFetchDroppedEnergy", "actionUnload"],
-  "scavenger" :["actionSetGivesEnergy","actionFetchDroppedEnergy", "actionUnload"],
+  "harvester": ["actionSetGivesEnergy", "actionHarvest", "actionDump"],
+  "harvestWithReturn": ["actionSetGivesEnergy", "actionHarvest", "actionUnload"],
+  "builder": ["actionSetWantsEnergy", "actionBuild"],
+  "controlUpgrader": ["actionSetWantsEnergy", "actionUpgradeController"],
+  "fortifier": ["actionSetWantsEnergy", "actionFortify"],
+  "repairer": ["actionSetWantsEnergy", "actionRepair"],
 
-  "scout":["actionMigrate","actionSign", "actionEnsureWatch"],
+  "harvestTruck": ["actionSetGivesEnergy", "actionUnload", "actionHarvestCollection"],
+  "resupplyBuildings": ["actionSetMovesEnergy", "actionDistributeToBuildings", "actionFetchFromStorage"],
+  "stockpile": ["actionSetMovesEnergy", "actionCollectRemote", "actionFillStorage"],
+  "resupplyWorkers": ["actionSetMovesEnergy", "actionDistributeToWorkers", "actionFetchFromStorage"],
+  "looter": ["actionSetGivesEnergy", "actionMigrate", "actionFetchRemote", "actionFetchDroppedEnergy", "actionUnload"],
+  "scavenger": ["actionSetGivesEnergy", "actionFetchDroppedEnergy", "actionUnload"],
 
-  "reserver":["actionMigrate","actionReserve","actionSign"],
-  "claimer":["actionMigrate","actionClaim","actionSign"],
-  
-  "defender": ["actionDefend","actionMigrate","actionHold","actionRecycle"],
-  "assaulter": ["actionRangedAssault","actionRally"],
-  
-  "healer": ["actionHealCreeps","actionHealerMove","actionRally"],
+  "scout": ["actionMigrate", "actionSign", "actionEnsureWatch"],
+
+  "reserver": ["actionMigrate", "actionReserve", "actionSign"],
+  "claimer": ["actionMigrate", "actionClaim", "actionSign"],
+
+  "defender": ["actionDefend", "actionMigrate", "actionHold", "actionRecycle"],
+  "assaulter": ["actionRangedAssault", "actionRally"],
+
+  "healer": ["actionHealCreeps", "actionHealerMove", "actionRally"],
   "recycler": ["actionRecycle"],
 
-  "remoteResupplyWorkers":["actionSetMovesEnergy","actionMigrate","actionDistributeToWorkers","actionFetchFromStorage"],
-  "remoteBuilder":["actionSetWantsEnergy","actionMigrate","actionBuild","actionRepair","actionFortify","actionFetchFromStorage"],
-  "remoteHarvester":["actionSetGivesEnergy","actionMigrate","actionEnsureDropPoint","actionResolveRemoteSourceFocus","actionHarvest","actionDump"],
-  "remoteCollector":["actionSetGivesEnergy","actionReturnOnFull","actionUnload","actionMigrate","actionResolveRemoteSourceFocus","actionHarvestCollection"],
-  
-  "pausedWorker":["actionSetGivesEnergy"],
-  "pausedMover":["actionSetGivesEnergy"]
+  "remoteResupplyWorkers": ["actionSetMovesEnergy", "actionMigrate", "actionDistributeToWorkers", "actionFetchFromStorage"],
+  "remoteBuilder": ["actionSetWantsEnergy", "actionMigrate", "actionBuild", "actionRepair", "actionFortify", "actionFetchFromStorage"],
+  "remoteHarvester": ["actionSetGivesEnergy", "actionMigrate", "actionEnsureDropPoint", "actionResolveRemoteSourceFocus", "actionHarvest", "actionDump"],
+  "remoteCollector": ["actionSetGivesEnergy", "actionReturnOnFull", "actionUnload", "actionMigrate", "actionResolveRemoteSourceFocus", "actionHarvestCollection"],
+
+  "pausedWorker": ["actionSetGivesEnergy"],
+  "pausedMover": ["actionSetGivesEnergy"]
 };
 
-var actions={};
-for (var roleName in  roleActions) {
+var actions = {};
+for (var roleName in roleActions) {
   var role = roleActions[roleName];
   for (var i = 0; i < role.length; i++) {
     var actionName = role[i];
-    if(typeof(actions[actionName])=="undefined") {
+    if (typeof (actions[actionName]) == "undefined") {
       actions[actionName] = require(actionName);
     }
   }
 }
 
 module.exports.loop = function () {
+  var logProfilerData = false;
   Memory.workingLinks = {};
   console.log("============= " + Game.time + " ==============");
   console.log("CPU bucket: " + Game.cpu.bucket);
@@ -58,52 +59,53 @@ module.exports.loop = function () {
   var lastTick = Game.cpu.getUsed();
   profilingData["aStart"] = lastTick;
 
-  
-  for(var name in Memory.creeps) {
-    if(!Game.creeps[name]) {
-        delete Memory.creeps[name];
-        console.log('Clearing memory:', name);
+
+  for (var name in Memory.creeps) {
+    if (!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      console.log('Clearing memory:', name);
     }
   }
 
-  for(var name in Memory.spawns) {
-    if(!Game.spawns[name]) {
-        delete Memory.spawns[name];
-        console.log('Clearing memory:', name);
+  for (var name in Memory.spawns) {
+    if (!Game.spawns[name]) {
+      delete Memory.spawns[name];
+      console.log('Clearing memory:', name);
     }
   }
-  for(var name in Memory.rooms) {
-    if(!Game.rooms[name]) {
-        delete Memory.rooms[name];
-        console.log('Clearing memory:', name);
+  for (var name in Memory.rooms) {
+    if (!Game.rooms[name]) {
+      delete Memory.rooms[name];
+      console.log('Clearing memory:', name);
     }
   }
   var lastTime = Game.cpu.getUsed();
-  console.log("before RoomLogic: " + (lastTime));
-  
-  console.log("(Game.time)");
-  console.log((Game.time));
-  console.log((Game.time % 10));
-  console.log((Game.time % 10) == 0);
-  if((Game.time % 10) == 0){ //roomLogic is way expensive, so only run it occasionally
+  if (logProfilerData)
+    console.log("before RoomLogic: " + (lastTime));
+
+  if ((Game.time % 10) == 0) { //roomLogic is way expensive, so only run it occasionally
+    console.log("Running rooms logic");
     roomLogic();
     var newTime = Game.cpu.getUsed();
-    console.log("roomlogic: " + (newTime - lastTime));
+    if (logProfilerData)
+      console.log("roomlogic: " + (newTime - lastTime));
     lastTime = newTime
   }
 
   runTowers();
   var newTime = Game.cpu.getUsed();
-  console.log("runTowers: " + (newTime - lastTime));
+  if (logProfilerData)
+    console.log("runTowers: " + (newTime - lastTime));
   lastTime = newTime
 
   runLinks();
   var newTime = Game.cpu.getUsed();
-  console.log("runLinks: " + (newTime - lastTime));
+  if (logProfilerData)
+    console.log("runLinks: " + (newTime - lastTime));
   lastTime = newTime
 
   creepLoop:
-  for(var creepName in Game.creeps){
+  for (var creepName in Game.creeps) {
     var creep = Game.creeps[creepName];
     actionEnsureHome(creep);
 
@@ -114,9 +116,10 @@ module.exports.loop = function () {
         var action = actions[actionsToTake[actionName]];
         if (action) {
           var actionResult = action(creep);
-          if(actionResult){
+          if (actionResult) {
             var newTime = Game.cpu.getUsed();
-            console.log(creep.name + ": " + actionsToTake[actionName] + " took " + (newTime - lastTime));
+            if (logProfilerData)
+              console.log(creep.name + ": " + actionsToTake[actionName] + " took " + (newTime - lastTime));
             lastTime = newTime
             break actionLoop;
           }
@@ -132,15 +135,15 @@ module.exports.loop = function () {
       lastTime = Game.cpu.getUsed();
     }
 
-    if(creep.memory.focus){
+    if (creep.memory.focus) {
       var focusObject = Game.getObjectById(creep.memory.focus);
-      if(focusObject)
+      if (focusObject)
         creep.room.visual.line(creep.pos, focusObject.pos);
     }
     lastTime = Game.cpu.getUsed();
 
     var usedCpu = Game.cpu.getUsed();
-    if(usedCpu > (Game.cpu.limit)){
+    if (usedCpu > (Game.cpu.limit)) {
       console.log("Quitting creep execution since used cpu time is " + usedCpu + " of " + Game.cpu.limit);
       break;
     }
