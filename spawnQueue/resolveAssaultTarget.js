efca3af9dabae8d851edcc8a669c241b1439160f
lsn <laustn@gmail.com>
2017-09-20 12:06:50 +0200

    move target resolution into separate file

---
+++ spawnQueue/resolveAssaultTarget.js
@@ -0,0 +1,61 @@
+var checkAssaultTarget = require("checkAssaultTarget");
+
+module.exports = function (creep) {
+  var flag = Game.flags[creep.memory.flag];
+  var target = null;
+
+  var sharedTarget = Game.getObjectById(creep.room.assaultTarget);
+  if (checkAssaultTarget(sharedTarget, flag)) {
+    target = sharedTarget;
+    console.log("shared target is " + JSON.stringify(target));
+  }
+
+  if (flag) {
+    if (flag.pos.roomName != creep.pos.roomName) {
+      creep.moveTo(flag);
+      return true; //start by moving to the same room as the target flag
+    }
+
+    if (!target) {
+      var structuresAtTarget = flag.pos.lookFor(LOOK_STRUCTURES);
+      if (structuresAtTarget.length > 0 && (!structuresAtTarget[0].my)) {
+        target = structuresAtTarget[0];
+      }
+    }
+  }
+  if (!target) {
+    var creepTarget = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (creepTarget) {
+      target = creepTarget;
+    }
+  }
+  if (!target) {
+    var towerTarget = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
+      filter: function (structure) {
+        if (structure.structureType == STRUCTURE_TOWER)
+          return true;
+      }
+    });
+    if (towerTarget) {
+      target = towerTarget;
+    }
+  }
+
+  if (!target) {
+    var spawnTarget = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
+      filter: function (structure) {
+        if (structure.structureType == STRUCTURE_SPAWN)
+          return true;
+      }
+    });
+    if (spawnTarget) {
+      target = spawnTarget;
+    }
+  }
+
+  if (!target) {
+    target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+  }
+
+  return target;
+}
