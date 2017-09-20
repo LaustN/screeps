dfea12490d492dc74713ef5635d99c0ee26803f1
lsn <laustn@gmail.com>
2017-09-20 13:49:33 +0200

    better sticky

--- spawnQueue/actionHealerMove.js
+++ spawnQueue/actionHealerMove.js
@@ -1,6 +1,6 @@
 module.exports = function(creep){
   var destinationCreep = creep.pos.findClosestByRange(FIND_MY_CREEPS, {filter:function(myBattleCreep){
-    return (myBattleCreep.getActiveBodyParts(HEAL) + myBattleCreep.getActiveBodyParts(ATTACK) + myBattleCreep.getActiveBodyParts(RANGED_ATTACK) );
+    return (myBattleCreep.getActiveBodyparts(HEAL) + myBattleCreep.getActiveBodyparts(ATTACK) + myBattleCreep.getActiveBodyparts(RANGED_ATTACK) );
   }});
   if(destinationCreep){
     creep.moveTo(destinationCreep);
