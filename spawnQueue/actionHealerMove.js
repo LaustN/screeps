46fb6db51cc0e4e4c8b4866d4ecafc4a30510d91
lsn <laustn@gmail.com>
2017-09-20 14:02:56 +0200

    do not stick to yourself

--- spawnQueue/actionHealerMove.js
+++ spawnQueue/actionHealerMove.js
@@ -1,13 +1,16 @@
-module.exports = function(creep){
-  var destinationCreep = creep.pos.findClosestByRange(FIND_MY_CREEPS, {filter:function(myBattleCreep){
-    return (myBattleCreep.getActiveBodyparts(HEAL) + myBattleCreep.getActiveBodyparts(ATTACK) + myBattleCreep.getActiveBodyparts(RANGED_ATTACK) );
-  }});
+module.exports = function (creep) {
+  var destinationCreep = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
+    filter: function (myBattleCreep) {
+      return
+      ((myBattleCreep.getActiveBodyparts(HEAL) + myBattleCreep.getActiveBodyparts(ATTACK) + myBattleCreep.getActiveBodyparts(RANGED_ATTACK)) > 0)
+        && (myBattleCreep.id != creep.id);
+    }
+  });
   console.log("destination creep: " + destinationCreep.name);
-  if(destinationCreep){
+  if (destinationCreep) {
     creep.moveTo(destinationCreep);
     return true;
   }
 
   return false;
 }
-  
