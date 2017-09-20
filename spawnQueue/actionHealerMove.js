2edae9ddb03eac5cef3fedc8ccb880aa52eaa1c5
lsn <laustn@gmail.com>
2017-09-20 13:44:54 +0200

    make healers trail attackers

--- spawnQueue/actionHealerMove.js
+++ spawnQueue/actionHealerMove.js
@@ -1,5 +1,12 @@
 module.exports = function(creep){
-  console.log("actionHealerMove not implemented");
+  var destinationCreep = creep.pos.findClosestByRange(FIND_MY_CREEPS, {filter:function(myBattleCreep){
+    return (myBattleCreep.getActiveBodyParts(HEAL) + myBattleCreep.getActiveBodyParts(ATTACK) + myBattleCreep.getActiveBodyParts(RANGED_ATTACK) );
+  }});
+  if(destinationCreep){
+    creep.moveTo(destinationCreep);
+    return true;
+  }
+
   return false;
 }
   
