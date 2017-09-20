b2a76d971dcdf6024b791175ffadcf493df7637a
lsn <laustn@gmail.com>
2017-09-20 14:00:51 +0200

    log destinationcreep name

--- spawnQueue/actionHealerMove.js
+++ spawnQueue/actionHealerMove.js
@@ -2,6 +2,7 @@ module.exports = function(creep){
   var destinationCreep = creep.pos.findClosestByRange(FIND_MY_CREEPS, {filter:function(myBattleCreep){
     return (myBattleCreep.getActiveBodyparts(HEAL) + myBattleCreep.getActiveBodyparts(ATTACK) + myBattleCreep.getActiveBodyparts(RANGED_ATTACK) );
   }});
+  console.log("destination creep: " + destinationCreep.name);
   if(destinationCreep){
     creep.moveTo(destinationCreep);
     return true;
