790f8d29aa4fc4d24be608a9972cf792bbc9452c
lsn <laustn@gmail.com>
2017-10-05 10:18:01 +0200

    poor scout, so much containerstruction to perform

--- spawnQueue/actionEnsureDropPoint.js
+++ spawnQueue/actionEnsureDropPoint.js
@@ -5,7 +5,7 @@ module.exports = function (creep) {
     if (isNextToFocus) {
       var nearbyContainers = focus.pos.findInRange(FIND_STRUCTURES, 1, { filter: { structureType: STRUCTURE_CONTAINER } });
       var nearbyConstructionSites = focus.pos.findInRange(FIND_CONSTRUCTION_SITES, 2);
-      if (nearbyConstructionSites.length > 0 && creep.carry[RESOURCE_ENERGY] > 0) {
+      if ((nearbyConstructionSites.length > 0) && (creep.carry[RESOURCE_ENERGY] > 0)) {
         creep.build(nearbyConstructionSites[0]);
       }
       if (nearbyContainers.length == 0) {
@@ -13,10 +13,10 @@ module.exports = function (creep) {
           creep.pos.createConstructionSite(STRUCTURE_CONTAINER);
         }
       }
-      var containersInNeedOfRepair = _.filter(nearbyContainers, function(container){
+      var containersInNeedOfRepair = _.filter(nearbyContainers, function (container) {
         return (container.hits < container.hitsMax);
       });
-      if(containersInNeedOfRepair.length > 0){
+      if (containersInNeedOfRepair.length > 0) {
         creep.repair(containersInNeedOfRepair[0]);
         console.log(creep.name + " is reapiring a nearby container at " + containersInNeedOfRepair[0].pos);
       }
