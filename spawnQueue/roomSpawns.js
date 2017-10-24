b924eb9c44e2fa9573bbad9fea4a65fbff72a7be
lsn <laustn@gmail.com>
2017-10-24 10:44:54 +0200

    check spawnqueue length in innerloop of spawns iteration, since it might be shortened to length 0 by previous sawn

--- spawnQueue/roomSpawns.js
+++ spawnQueue/roomSpawns.js
@@ -1,15 +1,15 @@
 module.exports = function (room) {
   var spawns = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_SPAWN } });
 
-  if (room.memory.spawnQueue && room.memory.spawnQueue.length) {
-    for (var spawnIndex in spawns) {
+  for (var spawnIndex in spawns) {
+    if (room.memory.spawnQueue && room.memory.spawnQueue.length) {
       var spawn = spawns[spawnIndex];
       if (spawn.spawning) {
         continue;
       }
       var spawnOrder = room.memory.spawnQueue[0];
 
-      var spawnMessage = spawn.createCreep(spawnOrder.body, spawnOrder.name, spawnOrder.memory );
+      var spawnMessage = spawn.createCreep(spawnOrder.body, spawnOrder.name, spawnOrder.memory);
       if (spawnMessage == spawnOrder.name) {
         room.memory.spawnQueue = _.drop(room.memory.spawnQueue, 1);
         continue;
