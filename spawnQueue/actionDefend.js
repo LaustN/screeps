ae1c549dfb96fc7efb721374f85e25d1126711a4
lsn <laustn@gmail.com>
2017-11-02 13:50:37 +0100

    defenders now get healing parts

--- spawnQueue/actionDefend.js
+++ spawnQueue/actionDefend.js
@@ -15,7 +15,9 @@ module.exports = function (creep) {
     target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
   }
 
+  var attackState = 0;
   if (target) {
+    attackState = 1;
     var targetRange = creep.pos.getRangeTo(target);
     if (targetRange > 3) {
       creep.moveTo(target);
@@ -23,11 +25,63 @@ module.exports = function (creep) {
     if (targetRange < 3) {
       console.log(creep.name + " might move away?");
     }
+
     if (creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3).length > 1) {
       creep.rangedMassAttack();
+      attackState = 3;
     } else {
       creep.rangedAttack(target);
+      attackState = 2;
     }
+  }
+
+  if(creep.getActiveBodyparts(HEAL)){
+    if(creep.hits < creep.hitsMax){
+      creep.heal(creep);
+      return true;
+    }
+    if(attackState==1){
+      //just moving to target, find wounded friends in range
+      var woundedFriendsNearby = creep.pos.findInRange(FIND_MY_CREEPS,3, {filter:function(otherCreep){
+        return otherCreep.hits < otherCreep.hitsMax;
+      }});
+      if(woundedFriendsNearby.length>0){
+        var woundedFriend = creep.pos.findClosestByRange(FIND_MY_CREEPS, {filter:function(otherCreep){
+          return otherCreep.hits < otherCreep.hitsMax;
+        }});
+        if(woundedFriend){
+          if(creep.pos.isNearTo(woundedFriend)){
+            creep.heal(woundedFriend);
+          }
+          else{
+            creep.rangedHeal(woundedFriend);
+          }
+        }
+        
+      }
+    }
+
+    if(attackState == 0){
+      var woundedFriend = creep.pos.findClosestByRange(FIND_MY_CREEPS, {filter:function(otherCreep){
+        return otherCreep.hits < otherCreep.hitsMax;
+      }});
+
+      if(woundedFriend){
+        if(creep.pos.isNearTo(woundedFriend)){
+          creep.heal(woundedFriend);
+        }
+        else{
+          creep.moveTo(woundedFriend);
+          creep.rangedHeal(woundedFriend);          
+        }
+        
+        //acting like a healer is also a valid action in itself
+        return true;
+      }
+    }
+  }
+
+  if(attackState){
     return true;
   }
 
