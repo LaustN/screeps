d08dc3f43265948be3049bb9daca67be3a6540e1
LaustN <laustn@gmail.com>
2017-11-04 13:44:52 +0100

    refuse targets not related to any roomObject

--- spawnQueue/checkAssaultTarget.js
+++ spawnQueue/checkAssaultTarget.js
@@ -1,11 +1,14 @@
-module.exports = function(target, roomObject){
-  if(target){
-    if(target.my)
+module.exports = function (target, roomObject) {
+  if (target) {
+    if (target.my)
       return false;
 
-    if(target.pos.roomName != roomObject.pos.roomName)
+    if (!roomObject)
       return false;
-    
+
+    if (target.pos.roomName != roomObject.pos.roomName)
+      return false;
+
     return true;
   }
   return false;
