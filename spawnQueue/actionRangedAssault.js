839ea28270283171aab4972f914965ea8c75b27e
LaustN <laustn@gmail.com>
2017-11-04 13:10:46 +0100

    count structures as targets, make optimal ranged distance 2 rather than 3

--- spawnQueue/actionRangedAssault.js
+++ spawnQueue/actionRangedAssault.js
@@ -6,13 +6,13 @@ module.exports = function (creep) {
   if (target) {
     creep.room.assaultTarget = target.id;
     var targetRange = creep.pos.getRangeTo(target);
-    if ((targetRange > 3))  {
+    if ((targetRange > 2))  {
       creep.moveTo(target);
     }
-    if (targetRange < 3) {
+    if (targetRange < 2) {
       creep.moveTo(Game.getObjectById(creep.memory.home));
     }
-    if (creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3).length > 1) {
+    if ((creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3).length + creep.pos.findInRange(FIND_HOSTILE_STRUCTURES, 3).length) > 1) {
       creep.rangedMassAttack();
     } else {
       creep.rangedAttack(target);
