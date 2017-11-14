1523ed5563308e730c105acf7c6f1e3dfdd912fb
lsn <laustn@gmail.com>
2017-11-14 10:46:00 +0100

    moving to keeper lair with lowest ticksToSpawn

--- spawnQueue/actionMoveToSourceKeeperLair.js
+++ spawnQueue/actionMoveToSourceKeeperLair.js
@@ -1,9 +1,16 @@
 module.exports = function (creep) {
 
   var keeperLairs = creep.room.find(FIND_STRUCTURES, {filter:{structureType: STRUCTURE_KEEPER_LAIR}});
+  var destination = null;
   if(keeperLairs.length){
-    console.log("found " + keeperLairs.length + " keeper lairs in " + creep.room.name);
+    destination = keeperLairs[0];
+    _.each(keeperLairs, function(keeperLair){
+      if(keeperLair.ticksToSpawn < destination.ticksToSpawn){
+        destination = keeperLair;
+      }
+    });
+    creep.moveTo(destination);
+    return true;
   }
-
   return false;
 }
