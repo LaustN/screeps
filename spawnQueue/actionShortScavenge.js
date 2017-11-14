dccb2cfb0eccd0732cd9742d760e2fc06bd436f5
lsn <laustn@gmail.com>
2017-11-14 11:32:32 +0100

    plausibly better short scavenge

--- spawnQueue/actionShortScavenge.js
+++ spawnQueue/actionShortScavenge.js
@@ -3,7 +3,7 @@ module.exports = function (creep) {
     return false;
 
   var droppedResourcesHere = creep.pos.findInRange(FIND_DROPPED_RESOURCES,1);
-  if(droppedResourcesHere.length > 0 && droppedResourcesHere[0].resourceType == RESOURCE_ENERGY)
+  if((droppedResourcesHere.length > 0) && (droppedResourcesHere[0].resourceType == RESOURCE_ENERGY))
     creep.pickup(droppedResourcesHere[0]);
   return false;
 }
