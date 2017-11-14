c94eeae7d2aaa3a3221fe639c2bfefb033046dbd
lsn <laustn@gmail.com>
2017-11-14 11:34:35 +0100

    more tweaking of short scavenge

--- spawnQueue/actionShortScavenge.js
+++ spawnQueue/actionShortScavenge.js
@@ -2,8 +2,10 @@ module.exports = function (creep) {
   if (creep.carryCapacity == _.sum(creep.carry))
     return false;
 
-  var droppedResourcesHere = creep.pos.findInRange(FIND_DROPPED_RESOURCES,1);
-  if((droppedResourcesHere.length > 0) && (droppedResourcesHere[0].resourceType == RESOURCE_ENERGY))
+  var droppedResourcesHere = creep.pos.findInRange(FIND_DROPPED_RESOURCES, 1);
+  if ((droppedResourcesHere.length > 0) && (droppedResourcesHere[0].resourceType == RESOURCE_ENERGY)) {
     creep.pickup(droppedResourcesHere[0]);
+    console.log(creep.name + " picking up energy at " + droppedResourcesHere[0].pos);
+  }
   return false;
 }
