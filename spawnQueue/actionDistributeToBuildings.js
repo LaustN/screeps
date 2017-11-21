6d15b0c2523dc1c156ec18d2f2fbf5cfba81f6a1
LaustN <laustn@gmail.com>
2017-11-21 20:28:10 +0100

    better test for structure being mine

--- spawnQueue/actionDistributeToBuildings.js
+++ spawnQueue/actionDistributeToBuildings.js
@@ -1,49 +1,54 @@
-module.exports = function(creep){
-  if(creep.carry[RESOURCE_ENERGY] > 0){
+module.exports = function (creep) {
+  if (creep.carry[RESOURCE_ENERGY] > 0) {
     var destination = Game.getObjectById(creep.memory.focus);
 
-    if(destination && (
-      !destination.energyCapacity  
-      || !((destination.energyCapacity - destination.energy)>0) )
-      || !destination.my    ) {
+    if (destination && (
+      !destination.energyCapacity
+      || !((destination.energyCapacity - destination.energy) > 0)
+      || !destination.my)
+    ) {
       destination = null;
       creep.memory.focus = null;
     }
 
-    if(!destination){
-      var unfilledExtension = creep.pos.findClosestByRange(FIND_MY_STRUCTURES,{filter: function(structure){
-        if(!(structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN))
-          return false;
-        if(structure.energyCapacity == structure.energy)
-          return false;
-        return true;
-      }});
+    if (!destination) {
+      var unfilledExtension = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+        filter: function (structure) {
+          if (!(structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN))
+            return false;
+          if (structure.energyCapacity == structure.energy)
+            return false;
+          return true;
+        }
+      });
 
-      if(unfilledExtension){
+      if (unfilledExtension) {
         destination = unfilledExtension;
         creep.memory.focus = destination.id;
       }
     }
 
-    if(!destination){
-      var unfilledWithCapacity = creep.pos.findClosestByRange(FIND_MY_STRUCTURES,{filter: function(structure){
-        if(structure.structureType == STRUCTURE_LINK)
-          return false;
-        if(!structure.energyCapacity)
-          return false;
-        if(structure.energyCapacity == structure.energy)
-          return false;
-        return true;
-      }});
+    if (!destination) {
+      var unfilledWithCapacity = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+        filter: function (structure) {
+          if (structure.structureType == STRUCTURE_LINK)
+            return false;
+          if (!structure.energyCapacity)
+            return false;
+          if (structure.energyCapacity == structure.energy)
+            return false;
+          return true;
+        }
+      });
 
-      if(unfilledWithCapacity){
+      if (unfilledWithCapacity) {
         destination = unfilledWithCapacity;
         creep.memory.focus = destination.id;
       }
     }
 
-    if(destination){
-      if(creep.transfer(destination, RESOURCE_ENERGY)!=OK){
+    if (destination) {
+      if (creep.transfer(destination, RESOURCE_ENERGY) != OK) {
         creep.moveTo(destination);
       }
       var otherDistributersHere = creep.pos.findInRange(FIND_MY_CREEPS, 1, {
@@ -54,7 +59,7 @@ module.exports = function(creep){
             && otherDistributer.id != creep.id; //do not count yourself as an extra feeded
         }
       });
-      if(otherDistributersHere.length){
+      if (otherDistributersHere.length) {
         creep.transfer(otherDistributersHere[0], RESOURCE_ENERGY);
         creep.say("->" + otherDistributersHere[0].name);
       }
@@ -63,4 +68,3 @@ module.exports = function(creep){
   }
   return false;
 }
-  
