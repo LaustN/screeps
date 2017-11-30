b8bfd1ffd1ba6144e61ae320b62ba262117837b0
lsn <laustn@gmail.com>
2017-11-30 09:32:52 +0100

    let harvesters pick up large loads when unload location is available

--- spawnQueue/actionShortScavenge.js
+++ spawnQueue/actionShortScavenge.js
@@ -3,8 +3,21 @@ module.exports = function (creep) {
     return false;
 
   var droppedResourcesHere = creep.pos.findInRange(FIND_DROPPED_RESOURCES, 1);
+  var nonFullContainersHere = creep.pos.findInRange(FIND_STRUCTURES,1,{filter:function(structure){
+    if(structure.structureType == STRUCTURE_CONTAINER){
+      if(_.sum(structure.store) < structure.storeCapacity){
+        return true;
+      }
+    }
+    return false;
+  }});
+
   if ((droppedResourcesHere.length > 0) && (droppedResourcesHere[0].resourceType == RESOURCE_ENERGY)) {
     var pickupSizeLimit = creep.getActiveBodyparts(WORK) * 2;
+
+    if(nonFullContainersHere.length>0){
+      pickupSizeLimit = 100000; //ought to be int.max, don't know what that's called in js
+    }
 
     var resourceToPickUp = _.find(droppedResourcesHere, function(resource){
       return (resource.amount<pickupSizeLimit);
