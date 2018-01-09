3a824705c0f05858f5140eb3d0e01b05513c86ed
lsn <laustn@gmail.com>
2018-01-09 13:59:50 +0100

    saying reserver result

--- spawnQueue/actionReserve.js
+++ spawnQueue/actionReserve.js
@@ -1,13 +1,15 @@
 module.exports = function (creep) {
   if (creep.room.controller) {
-    if (creep.pos.isNearTo(creep.room.controller)){
+    if (creep.pos.isNearTo(creep.room.controller)) {
       var reserveResult = creep.reserveController(creep.room.controller);
-      if(reserveResult == ERR_INVALID_TARGET){
+      creep.say(reserveResult);
+      if (reserveResult == ERR_INVALID_TARGET) {
         creep.attackController(creep.room.controller);
       }
     }
-    else
+    else {
       creep.moveTo(creep.room.controller);
+    }
     return true;
   }
   return false;
