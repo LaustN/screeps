9727045cca5e78eb146fe0cd15f2ccb64129345d
lsn <laustn@gmail.com>
2018-01-11 08:46:43 +0100

    auto-destroy ramparts when able

--- spawnQueue/roomBuildings.js
+++ spawnQueue/roomBuildings.js
@@ -1,82 +1,88 @@
+var getRoomPositionsAtRange = function (roomPosition, range, filter) {
+    var result = [];
+    for (var i = -range; i <= range; i++) {
+        result.push(new RoomPosition(roomPosition.x - range, roomPosition.y + i, roomPosition.roomName));
+    }
+    for (var i = (range - 1); i >= -(range - 1); i--) {
+        result.push(new RoomPosition(roomPosition.x - i, roomPosition.y + range, roomPosition.roomName));
+    }
+    for (var i = range; i >= -range; i--) {
+        result.push(new RoomPosition(roomPosition.x + range, roomPosition.y + i, roomPosition.roomName));
+    }
+    for (var i = (range - 1); i >= -(range - 1); i--) {
+        result.push(new RoomPosition(roomPosition.x + i, roomPosition.y - range, roomPosition.roomName));
+    }
+    if (typeof (filter) != "undefined") {
+        return _.filter(result, filter);
+    }
+    return result;
+}
+
+var spacyFilter = function (position) {
+    try {
+        if ((position.x < 2) || (position.y < 2) || (position.x > 48) || (position.y > 48))
+            return false;
+        var directions = [];
+        directions.push(new RoomPosition(position.x, position.y - 1, position.roomName));
+        directions.push(new RoomPosition(position.x, position.y + 1, position.roomName));
+        directions.push(new RoomPosition(position.x - 1, position.y, position.roomName));
+        directions.push(new RoomPosition(position.x + 1, position.y, position.roomName));
+
+        var myThings = position.look();
+        for (var thingIndex in myThings) {
+            var myThing = myThings[thingIndex];
+            if (myThing
+                && (
+                    ((myThing.type == LOOK_STRUCTURES) && (myThing.structure.structureType != STRUCTURE_ROAD)) ||
+                    myThing.type == LOOK_CONSTRUCTION_SITES ||
+                    (myThing.type == LOOK_TERRAIN && myThing.terrain == 'wall')
+                )
+            ) {
+                return false; // this space is blocked
+            }
+        }
+
+
+        var positionIsOk = true;
+        directions.forEach(function (neighbour) {
+            var neighbourIsOk = true;
+            var neighbourThings = neighbour.look();
+            for (var thingIndex in neighbourThings) {
+                var neighbourThing = neighbourThings[thingIndex];
+                if (neighbourThing
+                    && (
+                        (neighbourThing.type == LOOK_STRUCTURES && (neighbourThing[LOOK_STRUCTURES].structureType != STRUCTURE_ROAD)) ||
+                        neighbourThing.type == LOOK_CONSTRUCTION_SITES
+                    )
+
+                ) {
+                    neighbourIsOk = false
+                }
+            }
+
+            if (!neighbourIsOk) {
+                positionIsOk = false;
+            }
+        });
+        return positionIsOk;
+    }
+    catch (exception) {
+        console.log(exception);
+        return false;
+    }
+};
+
 module.exports = function (room) {
     if (!room.controller)
         return;
     if (!room.controller.my)
         return;
 
-    var getRoomPositionsAtRange = function (roomPosition, range, filter) {
-        var result = [];
-        for (var i = -range; i <= range; i++) {
-            result.push(new RoomPosition(roomPosition.x - range, roomPosition.y + i, roomPosition.roomName));
-        }
-        for (var i = (range - 1); i >= -(range - 1); i--) {
-            result.push(new RoomPosition(roomPosition.x - i, roomPosition.y + range, roomPosition.roomName));
-        }
-        for (var i = range; i >= -range; i--) {
-            result.push(new RoomPosition(roomPosition.x + range, roomPosition.y + i, roomPosition.roomName));
-        }
-        for (var i = (range - 1); i >= -(range - 1); i--) {
-            result.push(new RoomPosition(roomPosition.x + i, roomPosition.y - range, roomPosition.roomName));
-        }
-        if (typeof (filter) != "undefined") {
-            return _.filter(result, filter);
-        }
-        return result;
+    //do not waste ages demolishing ramparts
+    var hostileRamparts = room.find(FIND_HOSTILE_STRUCTURES, { filter: { structureType: STRUCTURE_RAMPART } });
+    for(var rampartIndex in hostileRamparts){
+        hostileRamparts[rampartIndex].destroy();
     }
-
-    var spacyFilter = function (position) {
-        try {
-            if ((position.x < 2) || (position.y < 2) || (position.x > 48) || (position.y > 48))
-                return false;
-            var directions = [];
-            directions.push(new RoomPosition(position.x, position.y - 1, position.roomName));
-            directions.push(new RoomPosition(position.x, position.y + 1, position.roomName));
-            directions.push(new RoomPosition(position.x - 1, position.y, position.roomName));
-            directions.push(new RoomPosition(position.x + 1, position.y, position.roomName));
-
-            var myThings = position.look();
-            for (var thingIndex in myThings) {
-                var myThing = myThings[thingIndex];
-                if (myThing
-                    && (
-                        (myThing.type == LOOK_STRUCTURES) ||
-                        myThing.type == LOOK_CONSTRUCTION_SITES ||
-                        (myThing.type == LOOK_TERRAIN && myThing.terrain == 'wall')
-                    )
-                ) {
-                    return false; // this space is blocked
-                }
-            }
-
-
-            var positionIsOk = true;
-            directions.forEach(function (neighbour) {
-                var neighbourIsOk = true;
-                var neighbourThings = neighbour.look();
-                for (var thingIndex in neighbourThings) {
-                    var neighbourThing = neighbourThings[thingIndex];
-                    if (neighbourThing
-                        && (
-                            (neighbourThing.type == LOOK_STRUCTURES && (neighbourThing[LOOK_STRUCTURES].structureType != STRUCTURE_ROAD)) ||
-                            neighbourThing.type == LOOK_CONSTRUCTION_SITES
-                        )
-
-                    ) {
-                        neighbourIsOk = false
-                    }
-                }
-
-                if (!neighbourIsOk) {
-                    positionIsOk = false;
-                }
-            });
-            return positionIsOk;
-        }
-        catch (exception) {
-            console.log(exception);
-            return false;
-        }
-    };
 
     room.visual.clear();
     var spawns = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_SPAWN } });
