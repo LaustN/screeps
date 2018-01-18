57b4adf15c4d820f8a9d70d1c1ae201678a15f62
lsn <laustn@gmail.com>
2018-01-18 14:46:01 +0100

    new mod

--- spawnQueue/roomBuildings2.js
+++ spawnQueue/roomBuildings2.js
@@ -122,9 +122,9 @@ module.exports = function (room) {
     for (var positionIndex in positions) {
       var position = positions[positionIndex];
       if (
-        (((position.x + position.y) % 3) != 2) 
+        (((position.x + position.y) % 4) != 0) 
         && 
-        (((position.x - position.y) % 3) != 2)
+        (((position.x - position.y) % 4) != 0)
       ) {
         room.visual.text(buildingNumber, position);
         buildingNumber++;
