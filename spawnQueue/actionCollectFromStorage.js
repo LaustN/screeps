fc2d54cfa7b3703178c1c8d5bf5414ff4efe02b1
lsn <laustn@gmail.com>
2018-01-23 09:49:37 +0100

    better collection code

--- spawnQueue/actionCollectFromStorage.js
+++ spawnQueue/actionCollectFromStorage.js
@@ -1,14 +1,23 @@
 module.exports = function (creep) {
-  var structuresWithStorage = source.pos.findClosestByRange(FIND_STRUCTURES, {
+  var structureWithStorage = source.pos.findClosestByRange(FIND_STRUCTURES, {
     filter: function (structure) {
-      if ((structure.storeCapacity > 0) && (structure.store[RESOURCE_ENERGY] > 0))
+      if ((structure.storeCapacity > 0) && (structure.store[RESOURCE_ENERGY] > creep.carryCapacity))
         return true;
       return false;
     }
   });
-  if (structuresWithStorage.length > 0) {
-    if (creep.withdraw(structuresWithStorage[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
-        creep.moveTo(structuresWithStorage[0]);
+  if (structureWithStorage == null) {
+    structureWithStorage = source.pos.findClosestByRange(FIND_STRUCTURES, {
+      filter: function (structure) {
+        if ((structure.storeCapacity > 0) && (structure.store[RESOURCE_ENERGY] > 0))
+          return true;
+        return false;
+      }
+    });
+  }
+  if (structureWithStorage != null) {
+    if (creep.withdraw(structureWithStorage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
+        creep.moveTo(structureWithStorage);
     }
     return true;
   }
